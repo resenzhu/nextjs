79b90522004e93c28393330d54d38ee30fddf579
Resen <resen.zhu@gmail.com>
2023-06-22 13:21:33 +0700

    add csp headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 'use strict';
 
+const {createSecureHeaders} = require('next-secure-headers');
 const withPlugins = require('next-compose-plugins');
+
+const development = process.env.NODE_ENV === 'development';
 
 const nextConfig =
 {
@@ -17,6 +20,39 @@ const nextHeaders =
 {
   headers: async () =>
   [
+    {
+      source: '/:path*',
+      headers: createSecureHeaders(
+      {
+        contentSecurityPolicy:
+        {
+          directives:
+          {
+            baseURI: ['\'self\''],
+            childSrc: ['\'self\''],
+            connectSrc: ['\'self\'', 'https://vitals.vercel-insights.com'],
+            defaultSrc: ['\'self\''],
+            fontSrc: ['\'self\''],
+            formAction: ['\'self\''],
+            frameSrc: ['\'self\''],
+            imgSrc: ['\'self\''],
+            manifestSrc: ['\'self\''],
+            mediaSrc: ['\'self\''],
+            objectSrc: ['\'self\''],
+            scriptSrc: ['\'self\'', 'https://va.vercel-scripts.com', ...(development ? ['\'unsafe-inline\'', '\'unsafe-eval\''] : [])],
+            styleSrc: ['\'self\''],
+            workerSrc: ['\'self\'']
+          }
+        },
+        expectCT: false,
+        forceHTTPSRedirect: false,
+        frameGuard: 'sameorigin',
+        noopen: 'noopen',
+        nosniff: 'nosniff',
+        referrerPolicy: 'no-referrer',
+        xssProtection: 'block-rendering'
+      })
+    },
     {
       source: '/:path*',
       headers:
