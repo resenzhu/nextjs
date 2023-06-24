660748097cb96e34ab24ae806f3eb269ec3783e7
Resen <resen.zhu@gmail.com>
2023-06-24 12:37:34 +0700

    add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,12 @@
+import {DateTime} from 'luxon';
+import type {MetadataRoute} from 'next';
+
+const Sitemap = (): MetadataRoute.Sitemap =>
+[
+  {
+    url: process.env.NEXT_PUBLIC_SITE_URL,
+    lastModified: DateTime.utc().toString()
+  }
+];
+
+export default Sitemap;
