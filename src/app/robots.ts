0cf8815d51a15ef298526f0129b0af6c91fdc0c5
Resen <resen.zhu@gmail.com>
2023-06-24 13:39:39 +0700

    add robots

---
+++ src/app/robots.ts
@@ -0,0 +1,16 @@
+import {MetadataRoute} from 'next';
+
+const Robots = (): MetadataRoute.Robots =>
+(
+  {
+    rules:
+    {
+      userAgent: '*',
+      allow: '/'
+    },
+    host: process.env.NEXT_PUBLIC_SITE_URL,
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`
+  }
+);
+
+export default Robots;
