86bd1b546a91858b99288a43ebbc7fc962f43779
Resen <resen.zhu@gmail.com>
2023-06-25 23:49:53 +0700

    add tailwind css

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @type {{plugins: {tailwindcss: object, autoprefixer: object}}}
+ **/
+const postcssConfig =
+{
+  plugins:
+  {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+};
+
+module.exports = postcssConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @type {import('tailwindcss').Config}
+ **/
+const tailwindConfig =
+{
+  content: [],
+  theme:
+  {
+    extend: {}
+  },
+  plugins: []
+};
+
+module.exports = tailwindConfig;
