8359600223803fee6313a17cec5f347d9eb83214
Resen <resen.zhu@gmail.com>
2023-06-26 20:39:27 +0700

    remove google font

--- src/utils/font.ts
+++
@@ -1,8 +0,0 @@
-import {Roboto_Flex as googleRobotoFlex} from 'next/font/google';
-
-export const robotoFlex = googleRobotoFlex(
-{
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-roboto-flex'
-});
