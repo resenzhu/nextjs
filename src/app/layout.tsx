0eb1e7d42f1022c951627beabe4dbb53eee39e22
resenzhu <resen.zhu@gmail.com>
2023-06-27 15:19:42 +0700

    remove navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import '@app/layout.css';
 import {Analytics} from '@vercel/analytics/react';
-import {Navbar} from '@components/main/navbar';
 import type {ReactNode} from 'react';
 import {rootMetadata as metadata} from '@utils/metadata';
 
@@ -13,11 +12,6 @@ const RootLayout = ({children}: RootLayoutProps): JSX.Element =>
 (
   <html lang='en'>
     <body>
-      <Navbar logo='/navbar/logo.webp' title='RESEN'>
-        <Navbar.Menu url='/about' label='About' />
-        <Navbar.Menu url='/portfolio' label='Portfolio' />
-        <Navbar.Menu url='/contact' label='Contact' />
-      </Navbar>
       {children}
       <Analytics />
     </body>
