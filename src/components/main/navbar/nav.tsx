882974f794d42de319f62f66ef493b3bc16d6b4b
Resen <resen.zhu@gmail.com>
2023-06-29 20:10:56 +0700

    add sidenav toggle state

--- src/components/main/navbar/nav.tsx
+++ src/components/main/navbar/nav.tsx
@@ -1,41 +1,58 @@
+'use client';
+
+import {type ReactNode, useState} from 'react';
 import {faEllipsisVertical, faXmark} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import type {ReactNode} from 'react';
 
 type NavProps = {
   children: ReactNode;
 };
 
-const Nav = ({children}: NavProps): JSX.Element => (
-  <>
-    <button
-      className='flex'
-      type='button'
-    >
-      <FontAwesomeIcon
-        className='w-6 text-2xl text-gray-500'
-        icon={faEllipsisVertical}
-      />
-    </button>
-    <div className='fixed left-0 top-0 h-screen w-screen bg-black opacity-40'></div>
-    <div className='fixed right-0 top-0 h-screen w-3/5 bg-white'>
-      <div className='mx-6 flex h-full py-6'>
-        <div className='flex flex-1 flex-col space-y-5'>{children}</div>
-        <div>
-          <button
-            className='align-sub'
-            type='button'
-          >
-            <FontAwesomeIcon
-              className='text-2xl text-gray-500 hover:text-red-500'
-              icon={faXmark}
-            />
-          </button>
-        </div>
-      </div>
-    </div>
-  </>
-);
+const Nav = ({children}: NavProps): JSX.Element => {
+  const [showSidenav, setShowSidenav] = useState<boolean>(false);
+
+  const handleToggleSidenav = (show: boolean): void => {
+    if (show !== showSidenav) {
+      setShowSidenav(show);
+    }
+  };
+
+  return (
+    <>
+      <button
+        className='flex'
+        type='button'
+        onClick={(): void => handleToggleSidenav(!showSidenav)}
+      >
+        <FontAwesomeIcon
+          className='w-6 text-2xl text-gray-500'
+          icon={faEllipsisVertical}
+        />
+      </button>
+      {showSidenav && (
+        <>
+          <div className='fixed left-0 top-0 h-screen w-screen bg-black opacity-40'></div>
+          <div className='fixed right-0 top-0 h-screen w-7/12 bg-white'>
+            <div className='mx-6 flex h-full py-6'>
+              <div className='flex flex-1 flex-col space-y-5'>{children}</div>
+              <div className='pt-1'>
+                <button
+                  type='button'
+                  onClick={(): void => handleToggleSidenav(!showSidenav)}
+                >
+                  <FontAwesomeIcon
+                    className='text-2xl text-gray-500 hover:text-red-500'
+                    icon={faXmark}
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
 
 export type {NavProps};
 export default Nav;
