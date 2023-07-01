00b7645423d62ac0fe0b28ae1313da2ca17d9740
Resen <resen.zhu@gmail.com>
2023-07-01 18:21:44 +0700

    add simple landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import {Avatar, Specialty} from '@components/home';
 import {createMetadata} from '@utils/metadata';
 
 const metadata = createMetadata({
@@ -7,7 +8,35 @@ const metadata = createMetadata({
   url: '/'
 });
 
-const Page = (): JSX.Element => <section>HELLO WORLD!</section>;
+const Page = (): JSX.Element => (
+  <section className='h-[calc(100vh-4rem)]'>
+    <div className='absolute h-[calc(100vh-4rem)] w-full bg-[url("/home/jumbotron.webp")] bg-cover bg-center bg-no-repeat brightness-50'></div>
+    <div className='relative mx-4 flex h-full justify-center'>
+      <div className='flex flex-col items-center justify-center'>
+        <div className='pb-8'>
+          <Avatar
+            src='/home/avatar1.webp'
+            easter='/home/avatar2.webp'
+          />
+        </div>
+        <div className='flex flex-col items-center space-y-1 text-white'>
+          <div className='text-lg font-semibold'>Hello, I am</div>
+          <div className='text-3xl font-extrabold'>RESEN</div>
+          <div className='text-xl font-bold italic text-yellow-400'>
+            <Specialty
+              titles={[
+                'FULL STACK DEVELOPER',
+                'FRONT END DEVELOPER',
+                'BACK END DEVELOPER',
+                'MIDDLEWARE DEVELOPER'
+              ]}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
 
 export {metadata};
 export default Page;
