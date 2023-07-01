9e91a6d9415ecc3195e2151260ad51f2c9fa1829
Resen <resen.zhu@gmail.com>
2023-07-01 18:20:06 +0700

    add home specialty

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,1 +1,2 @@
 export {default as Avatar} from '@components/home/avatar';
+export {default as Specialty} from '@components/home/specialty';
---
+++ src/components/home/specialty.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Typewriter from 'typewriter-effect';
+
+type SpecialtyProps = {
+  titles: string[];
+};
+
+const Specialty = ({titles}: SpecialtyProps): JSX.Element => (
+  <Typewriter
+    options={{
+      strings: titles,
+      autoStart: true,
+      loop: true,
+      devMode: process.env.NODE_ENV === 'development'
+    }}
+  />
+);
+
+export type {SpecialtyProps};
+export default Specialty;
