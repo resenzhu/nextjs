61d4764da49ef49f0d73e79e2d649924c5767746
Resen <resen.zhu@gmail.com>
2023-07-02 15:18:19 +0700

    close navbar when changing page

--- src/components/main/navbar/navmenu.tsx
+++ src/components/main/navbar/navmenu.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import type {HTMLAttributeAnchorTarget} from 'react';
 import Link from 'next/link';
+import useNavbar from '@hooks/main/use-navbar';
 
 type NavMenuProps = {
   url: string;
@@ -7,15 +10,26 @@ type NavMenuProps = {
   target?: HTMLAttributeAnchorTarget;
 };
 
-const NavMenu = ({url, label, target}: NavMenuProps) => (
-  <Link
-    className='w-0 text-lg font-bold text-gray-600 duration-150 hover:text-blue-500'
-    href={url}
-    target={target}
-  >
-    {label}
-  </Link>
-);
+const NavMenu = ({url, label, target}: NavMenuProps) => {
+  const {showSidenav, setShowSidenav} = useNavbar();
+
+  const handleToggleSidenav = (show: boolean) => {
+    if (show !== showSidenav) {
+      setShowSidenav(show);
+    }
+  };
+
+  return (
+    <Link
+      className='w-0 text-lg font-bold text-gray-600 duration-150 hover:text-blue-500'
+      href={url}
+      target={target}
+      onClick={(): void => handleToggleSidenav(false)}
+    >
+      {label}
+    </Link>
+  );
+};
 
 export type {NavMenuProps};
 export default NavMenu;
