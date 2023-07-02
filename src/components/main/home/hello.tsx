675a0db24249ba558f0557069c5a4228f6a331b8
Resen <resen.zhu@gmail.com>
2023-07-02 12:23:02 +0700

    add hello button

---
+++ src/components/main/home/hello.tsx
@@ -0,0 +1,10 @@
+const Hello = (): JSX.Element => (
+  <button
+    className='bg-blue-500 py-2 font-bold tracking-wider'
+    type='button'
+  >
+    HELLO
+  </button>
+);
+
+export default Hello;
