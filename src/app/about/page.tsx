8267727053bb31e0f1893ae0dbe8c361e7730ef9
resenzhu <resen.zhu@gmail.com>
2023-07-03 14:42:30 +0700

    add about page and jumbotron component

---
+++ src/app/about/page.tsx
@@ -0,0 +1,22 @@
+import {Jumbotron} from '@components/main/shared';
+import {createMetadata} from '@utils/metadata';
+
+const metadata = createMetadata({
+  title: 'About | Resen',
+  description:
+    'Meet Resen, a passionate web developer showcasing skills, projects, and experiences in the dynamic field of web development.',
+  url: '/about'
+});
+
+const Page = (): JSX.Element => (
+  <section>
+    <Jumbotron
+      title='about'
+      subtitle='Unveiling the Mind and Vision Behind the Website'
+      description='Step behind the curtain and delve into the mind of a digital enthusiast, as I divulge the story of my adventures in design and development.'
+    />
+  </section>
+);
+
+export {metadata};
+export default Page;
