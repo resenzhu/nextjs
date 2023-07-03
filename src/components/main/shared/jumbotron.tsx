83d7fb5cddbe6fc7725b6c46501d9d9c00d7efe5
resenzhu <resen.zhu@gmail.com>
2023-07-03 14:23:02 +0700

    update styles

--- src/components/main/shared/jumbotron.tsx
+++ src/components/main/shared/jumbotron.tsx
@@ -9,12 +9,12 @@ const Jumbotron = ({
   subtitle,
   description
 }: JumbotronProps): JSX.Element => (
-  <div className='h-[55vh]'>
-    <div className='absolute h-[55vh] w-full bg-[url("/images/main/shared/background.webp")] bg-cover bg-right bg-no-repeat brightness-50 contrast-125'></div>
-    <div className='absolute h-[55vh] w-full bg-gray-700 opacity-70'></div>
-    <div className='relative mx-4 flex h-full flex-col justify-between py-14 text-center'>
+  <div className='h-96'>
+    <div className='absolute h-96 w-full bg-[url("/images/main/shared/background.webp")] bg-cover bg-right bg-no-repeat brightness-50 contrast-125'></div>
+    <div className='absolute h-96 w-full bg-gray-700 opacity-70'></div>
+    <div className='relative mx-4 flex h-full flex-col justify-between py-12 text-center'>
       <div className='space-y-3'>
-        <div className='text-5xl font-extrabold tracking-wider text-yellow-300'>
+        <div className='text-4xl font-extrabold tracking-wider text-yellow-300'>
           {title}
         </div>
         <div className='text-lg font-bold italic text-gray-200'>{subtitle}</div>
