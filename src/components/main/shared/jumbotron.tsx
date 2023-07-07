97373afd24a218e3152e962615c1d9075b893966
Resen <resen.zhu@gmail.com>
2023-07-07 19:36:46 +0700

    change div to section and update background image

--- src/components/main/shared/jumbotron.tsx
+++ src/components/main/shared/jumbotron.tsx
@@ -9,8 +9,8 @@ const Jumbotron = ({
   subtitle,
   description
 }: JumbotronProps): JSX.Element => (
-  <div className='h-96'>
-    <div className='absolute h-96 w-full bg-[url("/images/main/shared/background.webp")] bg-cover bg-right bg-no-repeat brightness-50 contrast-125'></div>
+  <section className='h-96'>
+    <div className='absolute h-96 w-full bg-[url("/images/main/shared-jumbotron.webp")] bg-cover bg-right bg-no-repeat brightness-50 contrast-125'></div>
     <div className='absolute h-96 w-full bg-gray-700 opacity-70'></div>
     <div className='relative mx-4 flex h-full flex-col space-y-10 py-12 text-center'>
       <div className='space-y-2'>
@@ -23,7 +23,7 @@ const Jumbotron = ({
       </div>
       <div className='text-lg font-light text-white'>{description}</div>
     </div>
-  </div>
+  </section>
 );
 
 export type {JumbotronProps};
