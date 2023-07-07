0c0e773b342acd7b6778cd5544cb2d266866ec73
Resen <resen.zhu@gmail.com>
2023-07-07 20:07:55 +0700

    remove explore event

--- src/components/main/home/explore.tsx
+++ src/components/main/home/explore.tsx
@@ -1,25 +1,11 @@
-'use client';
-
-import useNavbar from '@hooks/main/use-navbar';
-
-const Explore = (): JSX.Element => {
-  const {showSidenav, setShowSidenav} = useNavbar();
-
-  const handleToggleSidenav = (show: boolean) => {
-    if (show !== showSidenav) {
-      setShowSidenav(show);
-    }
-  };
-
-  return (
-    <button
-      className='border-2 border-white py-2 font-bold tracking-wider duration-150 hover:border-blue-500 hover:bg-blue-500'
-      type='button'
-      onClick={(): void => handleToggleSidenav(true)}
-    >
-      EXPLORE
-    </button>
-  );
-};
+const Explore = (): JSX.Element =>
+(
+  <button
+    className='border-2 border-white py-2 font-bold tracking-wider duration-150 hover:border-blue-500 hover:bg-blue-500'
+    type='button'
+  >
+    EXPLORE
+  </button>
+);
 
 export default Explore;
