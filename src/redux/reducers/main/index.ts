9b8cd3f0d49e75c23b258175f5454c5a0dd4d713
Resen <resen.zhu@gmail.com>
2023-07-07 16:37:16 +0700

    add app reducer

--- src/redux/reducers/main/index.ts
+++ src/redux/reducers/main/index.ts
@@ -1,12 +1,12 @@
+import appReducer from '@redux/reducers/main/app';
 import {combineReducers} from '@reduxjs/toolkit';
 import {createPersistConfig} from '@utils/persist';
-import homeReducer from '@redux/reducers/main/home';
 import navbarReducer from '@redux/reducers/main/navbar';
 import {persistReducer} from 'redux-persist';
 
 const combinedReducers = combineReducers({
-  navbar: persistReducer(createPersistConfig({key: 'navbar'}), navbarReducer),
-  home: persistReducer(createPersistConfig({key: 'home'}), homeReducer)
+  app: persistReducer(createPersistConfig({key: 'app'}), appReducer),
+  navbar: persistReducer(createPersistConfig({key: 'navbar'}), navbarReducer)
 });
 
 export default combinedReducers;
