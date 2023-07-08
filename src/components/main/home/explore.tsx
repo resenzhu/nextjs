72dde2b4459e525d7e1090b3a4153d8acba8ee92
Resen <resen.zhu@gmail.com>
2023-07-08 12:36:13 +0700

    runp prettier

--- src/components/main/home/explore.tsx
+++ src/components/main/home/explore.tsx
@@ -1,5 +1,4 @@
-const Explore = (): JSX.Element =>
-(
+const Explore = (): JSX.Element => (
   <button
     className='border-2 border-white py-2 font-bold tracking-wider duration-150 hover:border-blue-500 hover:bg-blue-500'
     type='button'
