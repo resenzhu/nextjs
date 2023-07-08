72294d45657cfb66955acff3317a4307e65aa390
Resen <resen.zhu@gmail.com>
2023-07-08 14:47:03 +0700

    remove navbar reducer

--- src/redux/reducers/main/index.ts
+++ src/redux/reducers/main/index.ts
@@ -1,12 +1,10 @@
 import appReducer from '@redux/reducers/main/app';
 import {combineReducers} from '@reduxjs/toolkit';
 import {createPersistConfig} from '@utils/persist';
-import navbarReducer from '@redux/reducers/main/navbar';
 import {persistReducer} from 'redux-persist';
 
 const combinedReducers = combineReducers({
-  app: persistReducer(createPersistConfig({key: 'app'}), appReducer),
-  navbar: persistReducer(createPersistConfig({key: 'navbar'}), navbarReducer)
+  app: persistReducer(createPersistConfig({key: 'app'}), appReducer)
 });
 
 export default combinedReducers;
