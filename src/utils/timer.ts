a881f34097961d993c94e6faa9dd3da138252f2b
Resen <resen.zhu@gmail.com>
2023-07-08 16:52:54 +0700

    add timer util

---
+++ src/utils/timer.ts
@@ -0,0 +1,18 @@
+import {EffectCallback} from 'react';
+
+export const debounce = (
+  callback: (...args: any[]) => any,
+  delay: number
+): EffectCallback => {
+  let timer: number | NodeJS.Timeout = 0;
+
+  return (): void => {
+    clearTimeout(timer);
+
+    timer = setTimeout((): void => {
+      timer = 0;
+
+      callback.apply(null);
+    }, delay);
+  };
+};
