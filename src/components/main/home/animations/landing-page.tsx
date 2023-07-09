00f0d8c232d50b532b33a528f73cc72378206448
Resen <resen.zhu@gmail.com>
2023-07-10 01:37:37 +0700

    update animation

--- src/components/main/home/animations/landing-page.tsx
+++ src/components/main/home/animations/landing-page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import {AnimatePresence, motion} from 'framer-motion';
 import {ReactNode} from 'react';
 import useHome from '@hooks/main/use-home';
 
@@ -10,7 +11,19 @@ type LandingPageProps = {
 const LandingPage = ({children}: LandingPageProps): JSX.Element => {
   const {explore} = useHome();
 
-  return <>{!explore && children}</>;
+  return (
+    <AnimatePresence initial={false}>
+      {!explore && (
+        <motion.div
+          className='h-full'
+          key='landingPage'
+          exit={{opacity: 0, transition: {duration: 0.15}}}
+        >
+          {children}
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export type {LandingPageProps};
