23753ed8c906a8efe10cc3da9f987e7a08ab3efa
resenzhu <resen.zhu@gmail.com>
2023-07-10 16:38:32 +0700

    remove home components

--- src/components/main/home/index.tsx
+++
@@ -1,4 +0,0 @@
-export {default as Avatar} from '@components/main/home/avatar';
-export {default as Explore} from '@components/main/home/explore';
-export {default as Hello} from '@components/main/home/hello';
-export {default as Specialty} from '@components/main/home/specialty';
