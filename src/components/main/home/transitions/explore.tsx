b5779e6f17bb7a19abf567dd16164ae64d8f1065
Resen <resen.zhu@gmail.com>
2023-07-10 22:54:58 +0700

    add home transitions

---
+++ src/components/main/home/transitions/explore.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import {ReactNode} from 'react';
+import {Transition} from '@headlessui/react';
+import useHome from '@hooks/main/use-home';
+
+type ExploreProps = {
+  children: ReactNode;
+};
+
+const Explore = ({children}: ExploreProps): JSX.Element => {
+  const {explore} = useHome();
+
+  return (
+    <Transition
+      className='flex justify-center'
+      show={explore}
+      enter='duration-150 ease-out'
+      enterFrom='translate-y-full opacity-0'
+      enterTo='translate-y-0 opacity-100'
+      leave='duration-150 ease-in'
+      leaveFrom='translate-y-0 opacity-100'
+      leaveTo='translate-y-full opacity-0'
+    >
+      {children}
+    </Transition>
+  );
+};
+
+export type {ExploreProps};
+export default Explore;
---
+++ src/components/main/home/transitions/index.tsx
@@ -0,0 +1,2 @@
+export {default as TExplore} from '@components/main/home/transitions/explore';
+export {default as TLandingPage} from '@components/main/home/transitions/landing-page';
