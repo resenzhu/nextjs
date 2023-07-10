40ab9b980644b5ae0f7a1744fbededde24eee1db
resenzhu <resen.zhu@gmail.com>
2023-07-10 16:39:52 +0700

    set landing page to default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import {Avatar, Explore, Hello, Specialty} from '@components/main/home';
-import {LandingPage as AnimateLandingPage} from '@components/main/home/animations';
 import {createMetadata} from '@utils/metadata';
 
 const metadata = createMetadata({
@@ -13,43 +11,11 @@ const Page = (): JSX.Element => (
   <section className='h-[calc(100vh-4rem)] sm:h-[calc(100vh-3.5rem)]'>
     <div className='absolute h-[calc(100vh-4rem)] w-full bg-[url("/images/main/shared-jumbotron.webp")] bg-cover bg-center bg-no-repeat brightness-50 contrast-125 sm:h-[calc(100vh-3.5rem)]'></div>
     <div className='absolute h-[calc(100vh-4rem)] w-full bg-gray-700 opacity-70 sm:h-[calc(100vh-3.5rem)]'></div>
-    <AnimateLandingPage>
-      <div className='relative mx-4 flex h-full justify-center'>
-        <div className='flex flex-col items-center justify-center text-white'>
-          <div className='pb-8'>
-            <Avatar
-              src='/images/main/home-avatar-original.webp'
-              easterSrc='/images/main/home-avatar-easter.webp'
-            />
-          </div>
-          <div className='flex flex-col items-center pb-16'>
-            <div className='font-semibold tracking-wide text-gray-200'>
-              HELLO, I AM
-            </div>
-            <div className='text-3xl font-extrabold'>RESEN</div>
-            <div className='font-semibold tracking-wide text-gray-200'>
-              I AM A
-            </div>
-            <div className='text-xl font-bold text-yellow-300'>
-              <Specialty
-                titles={[
-                  'FULL STACK DEVELOPER',
-                  'FRONT END DEVELOPER',
-                  'BACK END DEVELOPER',
-                  'MIDDLEWARE DEVELOPER',
-                  'MOBILE LEGENDS PLAYER',
-                  'HORROR ENTHUSIAST'
-                ]}
-              />
-            </div>
-          </div>
-          <div className='flex w-36 flex-col space-y-3'>
-            <Hello />
-            <Explore />
-          </div>
-        </div>
-      </div>
-    </AnimateLandingPage>
+    <div className='relative mx-4 flex h-full justify-center items-center sm:mx-12 md:mx-16 lg:mx-20 flex-col sm:flex-row'>
+      <div>LANDINGPAGE</div>
+      <div>EXPLORE</div>
+      <div>HELLO</div>
+    </div>
   </section>
 );
 
