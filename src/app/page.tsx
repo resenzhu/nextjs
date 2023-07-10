b497ba03dc5e4f65c98e3f813463c59febd7492f
Resen <resen.zhu@gmail.com>
2023-07-10 22:55:25 +0700

    add landing page and explore transitions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import {TExplore, TLandingPage} from '@components/main/home/transitions';
 import {createMetadata} from '@utils/metadata';
 
 const metadata = createMetadata({
@@ -11,10 +12,11 @@ const Page = (): JSX.Element => (
   <section className='h-[calc(100vh-4rem)] sm:h-[calc(100vh-3.5rem)]'>
     <div className='absolute h-[calc(100vh-4rem)] w-full bg-[url("/images/main/shared-jumbotron.webp")] bg-cover bg-center bg-no-repeat brightness-50 contrast-125 sm:h-[calc(100vh-3.5rem)]'></div>
     <div className='absolute h-[calc(100vh-4rem)] w-full bg-gray-700 opacity-70 sm:h-[calc(100vh-3.5rem)]'></div>
-    <div className='relative mx-4 flex h-full justify-center items-center sm:mx-12 md:mx-16 lg:mx-20 flex-col sm:flex-row'>
-      <div>LANDINGPAGE</div>
-      <div>EXPLORE</div>
-      <div>HELLO</div>
+    <div className='relative mx-4 flex h-full justify-center items-center sm:mx-12 md:mx-16 lg:mx-20'>
+      <div className='flex flex-col sm:flex-row'>
+        <TLandingPage>LANDINGPAGE</TLandingPage>
+        <TExplore>EXPLORE</TExplore>
+      </div>
     </div>
   </section>
 );
