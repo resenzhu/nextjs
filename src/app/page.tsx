e44f3604692535f502405f1b97c132dd9a931f02
Resen <resen.zhu@gmail.com>
2023-07-12 00:25:53 +0700

    add profile description, buttons, and urls.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {createMetadata} from '@utils/metadata';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
 
 const metadata = createMetadata({
   title: 'Resen | Full Stack Developer',
@@ -12,7 +19,7 @@ const Page = (): JSX.Element => (
     <div className='flex h-full flex-col items-center'>
       <div className='relative h-1/5 w-full'>
         <div className='absolute h-full w-full bg-[url("/images/main/home-profile-background.webp")] bg-cover bg-top bg-no-repeat brightness-50 contrast-125'></div>
-        <div className='absolute h-full w-full bg-cyan-500 opacity-20'></div>
+        <div className='absolute h-full w-full bg-cyan-600 opacity-20'></div>
         <div className='relative flex h-full translate-y-1/2 items-center justify-center'>
           <img
             className='rounded-full border-8 contrast-125'
@@ -22,12 +29,46 @@ const Page = (): JSX.Element => (
           />
         </div>
       </div>
-      <div className='relative flex h-full w-full flex-col items-center pt-20'>
-        <div className='text-2xl font-extrabold tracking-wide text-gray-600'>
-          RESEN
+      <div className='relative flex h-full flex-col items-center justify-between pt-20'>
+        <div className='flex flex-1 flex-col items-center'>
+          <div className='text-2xl font-extrabold tracking-wide text-gray-600'>
+            RESEN
+          </div>
+          <div className='text-md font-extrabold text-cyan-600'>
+            FULL STACK DEVELOPER
+          </div>
+          <div className='w-4/5 pt-6 text-center text-gray-500'>
+            Analytical programmer with a strong problem-solving mindset, adept
+            at optimizing code for performance and efficiency.
+          </div>
+          <div className='flex w-36 flex-col space-y-2 pt-8'>
+            <button
+              className='border-2 border-cyan-600 bg-cyan-600 py-2 font-bold tracking-wider text-white active:bg-cyan-700'
+              type='button'
+            >
+              HELLO
+            </button>
+            <button
+              className='border-2 border-cyan-600 py-2 font-bold tracking-wider text-cyan-600 hover:bg-cyan-600 hover:text-white active:bg-cyan-700'
+              type='button'
+            >
+              EXPLORE
+            </button>
+          </div>
         </div>
-        <div className='text-md font-extrabold text-cyan-600'>
-          FULL STACK DEVELOPER
+        <div className='flex w-1/2 items-center justify-between pb-6'>
+          <a className='text-2xl text-cyan-600'>
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a className='text-2xl text-cyan-600'>
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a className='text-2xl text-cyan-600'>
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a className='text-2xl text-cyan-600'>
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
         </div>
       </div>
     </div>
