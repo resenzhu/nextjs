ff06bcbcb2fb8cddba5af1f069434e27a551ceac
Resen <resen.zhu@gmail.com>
2023-07-16 10:05:25 +0700

    add home profile hello component

---
+++ src/components/main/home/profile/hello.tsx
@@ -0,0 +1,10 @@
+const Hello = (): JSX.Element => (
+  <button
+    className='border-2 border-cyan-600 bg-cyan-600 py-2 font-bold tracking-wider text-white duration-150 active:bg-cyan-700'
+    type='button'
+  >
+    HELLO
+  </button>
+);
+
+export default Hello;
