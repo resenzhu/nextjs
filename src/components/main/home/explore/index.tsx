757e4833004f30769da135152cfdedb9ad03bbdb
Resen <resen.zhu@gmail.com>
2023-07-19 14:04:10 +0700

    add explore hover animation

--- src/components/main/home/explore/index.tsx
+++ src/components/main/home/explore/index.tsx
@@ -14,10 +14,10 @@ const Explore = (): JSX.Element => (
   <T>
     <div className='grid h-full grid-cols-2 gap-2 px-4 py-6'>
       <THome>
-        <Home className='relative h-full w-full'>
+        <Home className='group/home relative h-full w-full hover:shadow-lg'>
           <div className='absolute h-full w-full bg-[url("/images/main/home-explore-home.webp")] bg-cover bg-center bg-no-repeat brightness-50'></div>
           <div className='relative h-full'>
-            <div className='absolute bottom-0 px-2 py-1 text-sm font-bold text-white'>
+            <div className='absolute bottom-0 w-full px-2 py-1 text-start text-sm font-bold text-white duration-150 group-hover/home:bg-white group-hover/home:text-gray-700 group-hover/home:shadow-md'>
               HOME
             </div>
           </div>
@@ -25,12 +25,12 @@ const Explore = (): JSX.Element => (
       </THome>
       <TAbout>
         <Link
-          className='relative'
+          className='group/about relative'
           href='/about'
         >
           <div className='absolute h-full w-full bg-[url("/images/main/home-explore-about.webp")] bg-cover bg-center bg-no-repeat brightness-50'></div>
           <div className='relative h-full'>
-            <div className='absolute bottom-0 px-2 py-1 text-sm font-bold text-white'>
+            <div className='absolute bottom-0 w-full px-2 py-1 text-start text-sm font-bold text-white duration-150 group-hover/about:bg-white group-hover/about:text-gray-700 group-hover/about:shadow-md'>
               ABOUT
             </div>
           </div>
@@ -38,12 +38,12 @@ const Explore = (): JSX.Element => (
       </TAbout>
       <TPortfolio>
         <Link
-          className='relative col-span-2'
+          className='group/portfolio relative col-span-2'
           href='/portfolio'
         >
           <div className='absolute h-full w-full bg-[url("/images/main/home-explore-portfolio.webp")] bg-cover bg-center bg-no-repeat brightness-50'></div>
           <div className='relative h-full'>
-            <div className='absolute bottom-0 px-2 py-1 text-sm font-bold text-white'>
+            <div className='absolute bottom-0 w-full px-2 py-1 text-start text-sm font-bold text-white duration-150 group-hover/portfolio:bg-white group-hover/portfolio:text-gray-700 group-hover/portfolio:shadow-md'>
               PORTFOLIO
             </div>
           </div>
@@ -51,12 +51,12 @@ const Explore = (): JSX.Element => (
       </TPortfolio>
       <TResources>
         <Link
-          className='relative col-span-2'
+          className='group/resources relative col-span-2'
           href='/resources'
         >
           <div className='absolute h-full w-full bg-[url("/images/main/home-explore-resources.webp")] bg-cover bg-center bg-no-repeat brightness-50'></div>
           <div className='relative h-full'>
-            <div className='absolute bottom-0 px-2 py-1 text-sm font-bold text-white'>
+            <div className='absolute bottom-0 w-full px-2 py-1 text-start text-sm font-bold text-white duration-150 group-hover/resources:bg-white group-hover/resources:text-gray-700 group-hover/resources:shadow-md'>
               RESOURCES
             </div>
           </div>
@@ -64,12 +64,12 @@ const Explore = (): JSX.Element => (
       </TResources>
       <TContact>
         <Link
-          className='relative'
+          className='group/contact relative'
           href='/contact'
         >
           <div className='absolute h-full w-full bg-[url("/images/main/home-explore-contact.webp")] bg-cover bg-center bg-no-repeat brightness-50'></div>
           <div className='relative h-full'>
-            <div className='absolute bottom-0 px-2 py-1 text-sm font-bold text-white'>
+            <div className='absolute bottom-0 w-full px-2 py-1 text-start text-sm font-bold text-white duration-150 group-hover/contact:bg-white group-hover/contact:text-gray-700 group-hover/contact:shadow-md'>
               CONTACT
             </div>
           </div>
@@ -77,12 +77,12 @@ const Explore = (): JSX.Element => (
       </TContact>
       <TGitHub>
         <Link
-          className='relative'
+          className='group/github relative'
           href='https://github.com/resenzhu/nextjs'
         >
           <div className='absolute h-full w-full bg-[url("/images/main/home-explore-github.webp")] bg-cover bg-center bg-no-repeat brightness-50'></div>
           <div className='relative h-full'>
-            <div className='absolute bottom-0 px-2 py-1 text-sm font-bold text-white'>
+            <div className='absolute bottom-0 w-full px-2 py-1 text-start text-sm font-bold text-white duration-150 group-hover/github:bg-white group-hover/github:text-gray-700 group-hover/github:shadow-md'>
               GITHUB
             </div>
           </div>
