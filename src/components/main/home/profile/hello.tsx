f0a26ac45159d9ea33498f8088186efe3a9e6d14
Resen <resen.zhu@gmail.com>
2023-07-19 16:58:14 +0700

    add toggle chatbot event

--- src/components/main/home/profile/hello.tsx
+++ src/components/main/home/profile/hello.tsx
@@ -1,10 +1,28 @@
-const Hello = (): JSX.Element => (
-  <button
-    className='border-2 border-cyan-600 bg-cyan-600 py-2 font-bold tracking-wider text-white duration-150 active:bg-cyan-700'
-    type='button'
-  >
-    HELLO
-  </button>
-);
+'use client';
+
+import useHome from '@hooks/main/use-home';
+
+const Hello = (): JSX.Element => {
+  const {section, setSection} = useHome();
+
+  const handleToggleChatbot = (show: boolean): void => {
+    if (show !== section.chatbot) {
+      setSection({
+        ...section,
+        chatbot: show
+      });
+    }
+  };
+
+  return (
+    <button
+      className='border-2 border-cyan-600 bg-cyan-600 py-2 font-bold tracking-wider text-white duration-150 active:bg-cyan-700'
+      type='button'
+      onClick={(): void => handleToggleChatbot(true)}
+    >
+      HELLO
+    </button>
+  );
+};
 
 export default Hello;
