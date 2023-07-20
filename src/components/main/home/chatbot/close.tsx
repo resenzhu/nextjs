683a5ba6a61d5f4cfbf7863c1a9d81c483f22086
Resen <resen.zhu@gmail.com>
2023-07-21 02:12:24 +0700

    add chatbot close component

---
+++ src/components/main/home/chatbot/close.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faClose} from '@fortawesome/free-solid-svg-icons';
+import useHome from '@hooks/main/use-home';
+
+const Close = (): JSX.Element => {
+  const {section, setSection} = useHome();
+
+  const handleToggleChatbot = (show: boolean): void => {
+    if (show !== section.chatbot) {
+      setSection({
+        ...section,
+        chatbot: show
+      });
+    }
+  };
+
+  return (
+    <button
+      className='flex'
+      type='button'
+      onClick={(): void => handleToggleChatbot(false)}
+    >
+      <FontAwesomeIcon
+        className='w-6 text-2xl'
+        icon={faClose}
+      />
+    </button>
+  );
+};
+
+export default Close;
