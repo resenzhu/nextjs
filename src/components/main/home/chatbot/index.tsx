b60c98f0848314f77a677a7c1abb27d9d6d573a1
Resen <resen.zhu@gmail.com>
2023-07-21 02:12:53 +0700

    add chatbot component and its animations

---
+++ src/components/main/home/chatbot/index.tsx
@@ -0,0 +1,72 @@
+import {T, TBackdrop, TChatbot} from '@components/main/home/chatbot/transition';
+import Close from '@components/main/home/chatbot/close';
+
+type ChatbotProps = {
+  name: string;
+};
+
+const Chatbot = ({name}: ChatbotProps): JSX.Element => (
+  <T>
+    <div className='fixed bottom-0 z-10 h-full'>
+      <TBackdrop>
+        <div className='fixed h-full w-full bg-black'></div>
+      </TBackdrop>
+      <TChatbot>
+        <div className='fixed bottom-0 w-full bg-white'>
+          <div className='bg-cyan-600'>
+            <div className='mx-4 flex justify-between py-3 align-middle text-white'>
+              <div className='space-x-4'>
+                <span className='font-bold'>{name}</span>
+                <span className='bg-green-600 px-2 py-1'>online</span>
+              </div>
+              <Close />
+            </div>
+          </div>
+          <div className='h-80'>
+            <div className='mx-4 grid h-80 space-y-2 overflow-y-auto py-4'>
+              <div className='w-fit place-self-end bg-gray-200 px-3 py-1'>
+                Hello!
+              </div>
+              <div className='w-fit place-self-start bg-cyan-600 px-3 py-1 text-white'>
+                Hello
+              </div>
+              <div className='w-fit place-self-end bg-gray-200 px-3 py-1'>
+                I want to know about you.
+              </div>
+              <div className='w-fit bg-cyan-600 px-3 py-1 text-white'>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that it has a more-or-less
+                normal distribution of letters, as opposed to using 'Content
+                here, content here', making it look like readable English.
+              </div>
+              <div className='w-fit place-self-end bg-gray-200 px-3 py-1'>
+                The standard chunk of Lorem Ipsum used since the 1500s is
+                reproduced below for those interested.
+              </div>
+            </div>
+          </div>
+          <div className='border-t-2'>
+            <div className='mx-4 space-y-2 py-4 text-white'>
+              <div className='w-fit bg-cyan-600 px-3 py-1 duration-150 hover:bg-cyan-700'>
+                I want to know about you.
+              </div>
+              <div className='w-fit bg-cyan-600 px-3 py-1 duration-150 hover:bg-cyan-700'>
+                What projects do you have?
+              </div>
+              <div className='w-fit bg-cyan-600 px-3 py-1 duration-150 hover:bg-cyan-700'>
+                I want to know about you.
+              </div>
+              <div className='w-fit bg-cyan-600 px-3 py-1 duration-150 hover:bg-cyan-700'>
+                What projects do you have?
+              </div>
+            </div>
+          </div>
+        </div>
+      </TChatbot>
+    </div>
+  </T>
+);
+
+export type {ChatbotProps};
+export default Chatbot;
---
+++ src/components/main/home/chatbot/transition.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import {Fragment, ReactNode} from 'react';
+import {Transition} from '@headlessui/react';
+import useHome from '@hooks/main/use-home';
+
+export type TProps = {
+  children: ReactNode;
+};
+
+export const T = ({children}: TProps): JSX.Element => {
+  const {section} = useHome();
+
+  return (
+    <Transition
+      show={section.chatbot}
+      as={Fragment}
+    >
+      {children}
+    </Transition>
+  );
+};
+
+export const TBackdrop = ({children}: TProps): JSX.Element => (
+  <Transition.Child
+    as={Fragment}
+    enter='duration-150 ease-out'
+    enterFrom='opacity-0'
+    enterTo='opacity-60'
+    leave='duration-150 ease-in'
+    leaveFrom='opacity-60'
+    leaveTo='opacity-0'
+  >
+    {children}
+  </Transition.Child>
+);
+
+export const TChatbot = ({children}: TProps): JSX.Element => (
+  <Transition.Child
+    as={Fragment}
+    enter='duration-150 ease-out'
+    enterFrom='translate-y-full'
+    enterTo='translate-y-0'
+    leave='duration-150 ease-in'
+    leaveFrom='translate-y-0'
+    leaveTo='translate-y-full'
+  >
+    {children}
+  </Transition.Child>
+);
