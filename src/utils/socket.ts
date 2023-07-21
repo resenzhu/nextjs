0e37c9925362996e981912baba04de024fae0912
Resen <resen.zhu@gmail.com>
2023-07-21 22:35:54 +0700

    disable trailing slash

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -3,6 +3,7 @@ import {Manager} from 'socket.io-client';
 const socketManager = new Manager(process.env.APP_SERVER, {
   transports: ['websocket', 'polling'],
   rejectUnauthorized: process.env.NODE_ENV === 'production',
+  addTrailingSlash: false,
   autoConnect: false
 });
 
