7e10d5f8756c3b435db3039b7736a5dbf569dd9b
Resen <resen.zhu@gmail.com>
2023-07-22 23:02:07 +0700

    remove options component

--- src/components/main/home/chatbot/index.tsx
+++ src/components/main/home/chatbot/index.tsx
@@ -2,7 +2,6 @@ import {T, TBackdrop, TChatbot} from '@components/main/home/chatbot/transition';
 import Chat from '@components/main/home/chatbot/chat';
 import Close from '@components/main/home/chatbot/close';
 import Online from '@components/main/home/chatbot/online';
-import Options from '@components/main/home/chatbot/options';
 import {faClose} from '@fortawesome/free-solid-svg-icons';
 
 type ChatbotProps = {
@@ -16,7 +15,7 @@ const Chatbot = ({name}: ChatbotProps): JSX.Element => (
         <div className='fixed h-full w-full bg-black'></div>
       </TBackdrop>
       <TChatbot>
-        <div className='fixed bottom-0 w-full bg-white'>
+        <div className='fixed bottom-0 h-[85vh] w-full bg-white'>
           <div className='bg-cyan-600'>
             <div className='mx-4 flex justify-between py-3 align-middle text-white'>
               <div className='space-x-4'>
@@ -29,15 +28,8 @@ const Chatbot = ({name}: ChatbotProps): JSX.Element => (
               <Close icon={faClose} />
             </div>
           </div>
-          <div className='h-[50vh]'>
-            <div className='mx-4 grid h-80 space-y-2 overflow-y-auto py-4'>
-              <Chat />
-            </div>
-          </div>
-          <div className='border-t-2 border-gray-300'>
-            <div className='mx-4 space-y-2 py-4 text-white'>
-              <Options />
-            </div>
+          <div className='mx-4 block h-full space-y-2 overflow-y-auto pb-20 pt-4'>
+            <Chat />
           </div>
         </div>
       </TChatbot>
