10aa18eb13565418ca58dc17c6d14ab137437cc6
Resen <resen.zhu@gmail.com>
2023-07-22 12:04:32 +0700

    add app socket

--- environment.d.ts
+++ environment.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    APP_SERVER: string;
+    NEXT_PUBLIC_APP_SOCKET: string;
     NEXT_PUBLIC_APP_URL: string;
     NEXT_TELEMETRY_DISABLED: number;
   }
