2938865659e92b90817b81b57130c2fd6172922e
Resen <resen.zhu@gmail.com>
2023-07-23 23:30:22 +0700

    update chatbot

--- src/components/main/home/chatbot/chat.tsx
+++ src/components/main/home/chatbot/chat.tsx
@@ -2,13 +2,16 @@
 
 import {useEffect, useState} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {TTyping} from '@components/main/home/chatbot/transition';
 import {faEllipsisH} from '@fortawesome/free-solid-svg-icons';
 import {mainSocket} from '@utils/socket';
+import useApp from '@hooks/main/use-app';
 import useHome from '@hooks/main/use-home';
 
 const Chat = (): JSX.Element => {
-  const [rendered, setRendered] = useState<boolean>(false);
+  const {online} = useApp();
   const {chatbot, setChatbot} = useHome();
+  const [rendered, setRendered] = useState<boolean>(false);
 
   useEffect((): void => {
     if (!rendered) {
@@ -18,81 +21,25 @@ const Chat = (): JSX.Element => {
 
   useEffect((): void => {
     if (rendered) {
-      if (chatbot.chat.length === 0) {
-        if (!chatbot.typing) {
-          setChatbot({...chatbot, typing: true});
-        }
-        if (mainSocket.connected) {
-          mainSocket.emit('ask-chatbot');
-        }
+      if (online && chatbot.chat.length === 0) {
+        setChatbot({...chatbot, typing: true});
+        mainSocket.emit('ask-chatbot', (response: any): void => {
+          setChatbot({...chatbot, typing: false});
+        });
       }
     }
-  }, [rendered]);
+  }, [rendered, online]);
 
   return (
     <>
-      {/* <div className='w-fit place-self-end bg-gray-200 px-3 py-1'>Hello!</div>
-      <div className='w-fit place-self-start bg-cyan-600 px-3 py-1 text-white'>
-        Hello
-      </div>
-      <div className='w-fit place-self-end bg-gray-200 px-3 py-1'>
-        I want to know about you.
-      </div>
-      <div className='w-fit bg-cyan-600 px-3 py-1 text-white'>
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters, as opposed to using Content here, content here, making it look
-        like readable English.
-      </div>
-      <div className='w-fit place-self-end bg-gray-200 px-3 py-1'>
-        The standard chunk of Lorem Ipsum used since the 1500s is reproduced
-        below for those interested.
-      </div>
-      <div className='w-fit place-self-end bg-gray-200 px-3 py-1'>Hello!</div>
-      <div className='w-fit place-self-start bg-cyan-600 px-3 py-1 text-white'>
-        Hello
-      </div>
-      <div className='w-fit place-self-end bg-gray-200 px-3 py-1'>
-        I want to know about you.
-      </div>
-      <div className='w-fit bg-cyan-600 px-3 py-1 text-white'>
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters, as opposed to using Content here, content here, making it look
-        like readable English.
-      </div>
-      <div className='w-fit place-self-end bg-gray-200 px-3 py-1'>
-        The standard chunk of Lorem Ipsum used since the 1500s is reproduced
-        below for those interested.
-      </div>
-      <div className='w-fit place-self-end bg-gray-200 px-3 py-1'>Hello!</div>
-      <div className='w-fit place-self-start bg-cyan-600 px-3 py-1 text-white'>
-        Hello
-      </div>
-      <div className='w-fit place-self-end bg-gray-200 px-3 py-1'>
-        I want to know about you.
-      </div>
-      <div className='w-fit bg-cyan-600 px-3 py-1 text-white'>
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters, as opposed to using Content here, content here, making it look
-        like readable English.
-      </div>
-      <div className='w-fit place-self-end bg-gray-200 px-3 py-1'>
-        The standard chunk of Lorem Ipsum used since the 1500s is reproduced
-        below for those interested.
-      </div> */}
-      {chatbot.typing && (
-        <div className='w-fit animate-fade-right place-self-start bg-cyan-600 px-3 py-1 text-white animate-duration-1000'>
+      <TTyping>
+        <div className='w-fit max-w-[80vw] place-self-start bg-cyan-600 px-3 py-1 text-white'>
           <FontAwesomeIcon
             className='animate-pulse animate-duration-1000 animate-infinite animate-ease-linear'
             icon={faEllipsisH}
           />
         </div>
-      )}
+      </TTyping>
     </>
   );
 };
