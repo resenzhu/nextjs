eafe453fb12bf9da552b991f633e66b39ea62423
Resen <resen.zhu@gmail.com>
2023-07-23 17:20:16 +0700

    add offline chat transition

--- src/components/main/home/chatbot/transition.tsx
+++ src/components/main/home/chatbot/transition.tsx
@@ -2,6 +2,7 @@
 
 import {Fragment, ReactNode} from 'react';
 import {Transition} from '@headlessui/react';
+import useApp from '@hooks/main/use-app';
 import useHome from '@hooks/main/use-home';
 
 export type TProps = {
@@ -48,3 +49,22 @@ export const TChatbot = ({children}: TProps): JSX.Element => (
     {children}
   </Transition.Child>
 );
+
+export const TOffline = ({children}: TProps): JSX.Element => {
+  const {online} = useApp();
+
+  return (
+    <Transition
+      show={!online}
+      as={Fragment}
+      enter='duration-150 ease-out'
+      enterFrom='-translate-x-full opacity-0'
+      enterTo='translate-x-0 opacity-100'
+      leave='duration-150 ease-in'
+      leaveFrom='translate-x-0 opacity-100'
+      leaveTo='-translate-x-full opacity-0'
+    >
+      {children}
+    </Transition>
+  );
+};
