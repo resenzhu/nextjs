16fefb94a3247a200dc079ce17fa816bb890157e
Resen <resen.zhu@gmail.com>
2023-07-23 13:10:39 +0700

    add webpack config

--- next.config.js
+++ next.config.js
@@ -20,7 +20,16 @@ const nextConfig = {
   },
   generateEtags: false,
   poweredByHeader: false,
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config, {isServer}) => {
+    if (isServer) {
+      config.externals.push({
+        bufferutil: 'bufferutil',
+        'utf-8-validate': 'utf-8-validate'
+      });
+    }
+    return config;
+  }
 };
 
 /**
