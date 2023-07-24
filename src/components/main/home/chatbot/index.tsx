ee7cd79d5675e218baac9345e9c3f6e0f463d2a7
resenzhu <resen.zhu@gmail.com>
2023-07-24 10:34:07 +0700

    add message component

--- src/components/main/home/chatbot/index.tsx
+++ src/components/main/home/chatbot/index.tsx
@@ -1,7 +1,8 @@
 import {T, TBackdrop, TChatbot} from '@components/main/home/chatbot/transition';
+import {faClose, faPaperPlane} from '@fortawesome/free-solid-svg-icons';
 import Chat from '@components/main/home/chatbot/chat';
 import Close from '@components/main/home/chatbot/close';
-import {faClose} from '@fortawesome/free-solid-svg-icons';
+import Message from '@components/main/home/chatbot/message';
 
 type ChatbotProps = {
   name: string;
@@ -14,15 +15,21 @@ const Chatbot = ({name}: ChatbotProps): JSX.Element => (
         <div className='fixed h-full w-full bg-black'></div>
       </TBackdrop>
       <TChatbot>
-        <div className='fixed bottom-0 h-[85vh] w-full bg-white'>
+        <div className='fixed bottom-0 flex h-[85vh] w-full flex-col bg-white'>
           <div className='bg-cyan-600'>
             <div className='mx-4 flex justify-between py-3 align-middle text-white'>
               <span className='font-bold'>{name}</span>
               <Close icon={faClose} />
             </div>
           </div>
-          <div className='mx-4 block h-full space-y-2 overflow-y-auto pb-20 pt-4'>
+          <div className='mx-4 grid h-full flex-1 space-y-2 overflow-y-auto py-4'>
             <Chat />
+          </div>
+          <div className='border-t-2'>
+            <Message
+              placeholder='Type a message'
+              sendIcon={faPaperPlane}
+            />
           </div>
         </div>
       </TChatbot>
