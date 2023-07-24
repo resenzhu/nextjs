d593bc0103d9fbfebbbb5985c835fab63b3798b3
Resen <resen.zhu@gmail.com>
2023-07-24 21:58:09 +0700

    read chat from state

--- src/components/main/home/chatbot/chat.tsx
+++ src/components/main/home/chatbot/chat.tsx
@@ -3,84 +3,43 @@
 import {TOffline, TTyping} from '@components/main/home/chatbot/transition';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faEllipsisH} from '@fortawesome/free-solid-svg-icons';
+import useHome from '@hooks/main/use-home';
 
-const Chat = (): JSX.Element => (
-  <>
-    {/* <div className='max-w-5/6 w-fit place-self-end bg-gray-200 px-3 py-1'>
-      Hello!
-    </div>
-    <div className='max-w-5/6 w-fit place-self-start bg-cyan-600 px-3 py-1 text-white'>
-      Hello
-    </div>
-    <div className='max-w-5/6 w-fit place-self-end bg-gray-200 px-3 py-1'>
-      I want to know about you.
-    </div>
-    <div className='max-w-5/6 w-fit bg-cyan-600 px-3 py-1 text-white'>
-      It is a long established fact that a reader will be distracted by the
-      readable content of a page when looking at its layout. The point of using
-      Lorem Ipsum is that it has a more-or-less normal distribution of letters,
-      as opposed to using Content here, content here, making it look like
-      readable English.
-    </div>
-    <div className='max-w-5/6 w-fit place-self-end bg-gray-200 px-3 py-1'>
-      The standard chunk of Lorem Ipsum used since the 1500s is reproduced below
-      for those interested.
-    </div>
-    <div className='max-w-5/6 w-fit place-self-end bg-gray-200 px-3 py-1'>
-      Hello!
-    </div>
-    <div className='max-w-5/6 w-fit place-self-start bg-cyan-600 px-3 py-1 text-white'>
-      Hello
-    </div>
-    <div className='max-w-5/6 w-fit place-self-end bg-gray-200 px-3 py-1'>
-      I want to know about you.
-    </div>
-    <div className='max-w-5/6 w-fit bg-cyan-600 px-3 py-1 text-white'>
-      It is a long established fact that a reader will be distracted by the
-      readable content of a page when looking at its layout. The point of using
-      Lorem Ipsum is that it has a more-or-less normal distribution of letters,
-      as opposed to using Content here, content here, making it look like
-      readable English.
-    </div>
-    <div className='max-w-5/6 w-fit place-self-end bg-gray-200 px-3 py-1'>
-      The standard chunk of Lorem Ipsum used since the 1500s is reproduced below
-      for those interested.
-    </div>
-    <div className='max-w-5/6 w-fit place-self-end bg-gray-200 px-3 py-1'>
-      Hello!
-    </div>
-    <div className='max-w-5/6 w-fit place-self-start bg-cyan-600 px-3 py-1 text-white'>
-      Hello
-    </div>
-    <div className='max-w-5/6 w-fit place-self-end bg-gray-200 px-3 py-1'>
-      I want to know about you.
-    </div>
-    <div className='max-w-5/6 w-fit bg-cyan-600 px-3 py-1 text-white'>
-      It is a long established fact that a reader will be distracted by the
-      readable content of a page when looking at its layout. The point of using
-      Lorem Ipsum is that it has a more-or-less normal distribution of letters,
-      as opposed to using Content here, content here, making it look like
-      readable English.
-    </div>
-    <div className='max-w-5/6 w-fit place-self-end bg-gray-200 px-3 py-1'>
-      The standard chunk of Lorem Ipsum used since the 1500s is reproduced below
-      for those interested.
-    </div> */}
-    <TOffline>
-      <div className='w-fit max-w-[80vw] place-self-start bg-cyan-600 px-3 py-1 text-white'>
-        Oops! It appears that you're currently offline. Please ensure that
-        you're connected to the internet and try again later.
-      </div>
-    </TOffline>
-    <TTyping>
-      <div className='w-fit max-w-[80vw] place-self-start bg-cyan-600 px-3 py-1 text-white'>
-        <FontAwesomeIcon
-          className='animate-pulse animate-duration-1000 animate-infinite animate-ease-linear'
-          icon={faEllipsisH}
-        />
-      </div>
-    </TTyping>
-  </>
-);
+const Chat = (): JSX.Element => {
+  const {chatbot} = useHome();
+
+  return (
+    <>
+      {chatbot.chat.map(
+        (chat, index): JSX.Element => (
+          <div
+            className={
+              chat.sender === 'bot'
+                ? 'max-w-5/6 w-fit place-self-start bg-cyan-600 px-3 py-1 text-white'
+                : 'max-w-5/6 w-fit place-self-end bg-gray-200 px-3 py-1'
+            }
+            key={index}
+          >
+            {chat.message}
+          </div>
+        )
+      )}
+      <TOffline>
+        <div className='w-fit max-w-[80vw] place-self-start bg-cyan-600 px-3 py-1 text-white'>
+          Oops! It appears that you're currently offline. Please ensure that
+          you're connected to the internet and try again later.
+        </div>
+      </TOffline>
+      <TTyping>
+        <div className='w-fit max-w-[80vw] place-self-start bg-cyan-600 px-3 py-1 text-white'>
+          <FontAwesomeIcon
+            className='animate-pulse animate-duration-1000 animate-infinite animate-ease-linear'
+            icon={faEllipsisH}
+          />
+        </div>
+      </TTyping>
+    </>
+  );
+};
 
 export default Chat;
