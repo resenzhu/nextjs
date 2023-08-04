b68711e1d272ec7f7c34487dcb3ace3a3f3bdb05
Resen <resen.zhu@gmail.com>
2023-08-04 14:45:25 +0700

    add resources page

---
+++ src/app/resources/page.tsx
@@ -0,0 +1,13 @@
+import {createMetadata} from '@utils/metadata';
+
+const metadata = createMetadata({
+  title: 'Resources | Resen',
+  description:
+    'Discover a range of downloadable guides, tutorials, and other enriching resources for your benefit.',
+  url: '/resources'
+});
+
+const Page = (): JSX.Element => <>RESOURCES</>;
+
+export {metadata};
+export default Page;
