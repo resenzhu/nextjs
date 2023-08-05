40865ff836614b797f2b2ee2863aa3c675eae627
Resen <resen.zhu@gmail.com>
2023-08-05 23:30:47 +0700

    add main header component

---
+++ src/components/main/shared/header.tsx
@@ -0,0 +1,28 @@
+type HeaderProps = {
+  title: string;
+  subtitle: string;
+  description: string;
+};
+
+const Header = ({title, subtitle, description}: HeaderProps): JSX.Element => (
+  <div className='relative grid h-80'>
+    <div className='absolute h-full w-full bg-[url("/images/main/home-profile-background.webp")] bg-cover bg-center bg-no-repeat brightness-50 contrast-125'></div>
+    <div className='absolute h-full w-full bg-cyan-600 opacity-40'></div>
+    <div className='relative mx-4 flex flex-col space-y-6 self-center text-center'>
+      <div className='space-y-1'>
+        <div className='text-3xl font-extrabold tracking-widest text-yellow-400'>
+          {title}
+        </div>
+        <div className='mx-auto w-5/6 text-lg font-bold italic text-gray-100'>
+          {subtitle}
+        </div>
+      </div>
+      <div className='text-md mx-auto w-5/6 flex-1 font-light text-white'>
+        {description}
+      </div>
+    </div>
+  </div>
+);
+
+export type {HeaderProps};
+export default Header;
