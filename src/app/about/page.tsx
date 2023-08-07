fb2b657037e598e777295bbde06632c9441f6faa
resenzhu <resen.zhu@gmail.com>
2023-08-07 17:00:29 +0700

    add about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,13 +9,21 @@ const metadata = createMetadata({
 });
 
 const Page = (): JSX.Element => (
-  <section>
-    <Header
-      title='about'
-      subtitle='Unveiling the Mind and Vision Behind the Website'
-      description='Step behind the curtain and delve into the mind of a digital enthusiast, as I divulge the story of my adventures in design and development.'
-    />
-  </section>
+  <>
+    <section>
+      <Header
+        title='about'
+        subtitle='Unveiling the Mind and Vision Behind the Website'
+        description='Step behind the curtain and delve into the mind of a digital enthusiast, as I divulge the story of my adventures in design and development.'
+      />
+    </section>
+    <section className='h-[calc(100vh-4rem)] md:h-[calc(100vh-3.5rem)]'>
+      <div className='flex flex-col items-center'>
+        <div className='text-xl font-extrabold text-cyan-600'>ABOUT ME</div>
+        <div className='flex-1'>Description</div>
+      </div>
+    </section>
+  </>
 );
 
 export {metadata};
