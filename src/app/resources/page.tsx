276d68bae631e30601cb353137d8c70943f12ee6
resenzhu <resen.zhu@gmail.com>
2023-08-07 11:34:06 +0700

    add header

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,3 +1,4 @@
+import {Header} from '@components/main/shared';
 import {createMetadata} from '@utils/metadata';
 
 const metadata = createMetadata({
@@ -7,7 +8,15 @@ const metadata = createMetadata({
   url: '/resources'
 });
 
-const Page = (): JSX.Element => <>RESOURCES</>;
+const Page = (): JSX.Element => (
+  <section>
+    <Header
+      title='resources'
+      subtitle='Your Gateway to Endless Inspiration and Learning'
+      description='Explore a world of knowledge with my extensive collection of downloadable resources. From guides to tutorials, my carefully curated selection covers various interests and disciplines, empowering your learning journey.'
+    />
+  </section>
+);
 
 export {metadata};
 export default Page;
