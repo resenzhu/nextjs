b5d98de30840be7d0b85c1a1bf4836ab4a6b52e0
resenzhu <resen.zhu@gmail.com>
2023-08-09 09:55:07 +0700

    update about me description and add animation

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,26 +19,28 @@ const Page = (): JSX.Element => (
     </section>
     <section className='h-[calc(100vh-4rem)] min-h-[calc(100vh-4rem)] md:h-[calc(100vh-3.5rem)] md:min-h-[calc(100vh-3.5rem)]'>
       <div className='mx-4 flex flex-col items-center space-y-6 py-8'>
-        <div className='text-2xl font-extrabold text-cyan-600'>ABOUT ME</div>
+        <div className='animate-fade-right text-2xl font-extrabold text-cyan-600 animate-duration-700'>
+          ABOUT ME
+        </div>
         <div className='flex-1 animate-fade-up space-y-6 text-center text-gray-600 animate-duration-700'>
           <p>
-            Welcome to my personal portfolio website! My name is Resen and I am
-            a web developer based in Jakarta. With a strong focus on both
-            front-end and back-end development, I have gained extensive
-            experience in creating dynamic web applications.
+            Welcome to my personal portfolio website! My name is Resen, and I&#39;m
+            a Full Stack developer based in Jakarta with over five years of
+            experience in the field.
           </p>
           <p>
-            With over 5 years of experience in the field, I have honed my skills
-            and developed a deep understanding of web development principles and
-            best practices. Throughout my career, I have worked on various
-            projects, ranging from small to complex web applications.
+            I specialize in both Front End and Back End development, bringing a
+            comprehensive skill set to the table. With a strong focus on
+            creating visually appealing and user-friendly web applications, I
+            utilize technologies such as ReactJS, NextJS, and NodeJS to deliver
+            exceptional results.
           </p>
           <p>
-            I am passionate about creating user-friendly and visually appealing
-            websites that provide a seamless browsing experience. By leveraging
-            the power of React.js, Next.js, and Node.js, I strive to deliver
-            high-quality code that not only meets client requirements but also
-            adheres to industry standards.
+            From crafting captivating user interfaces to implementing robust
+            functionality, I strive to create seamless and efficient websites.
+            My passion for development drives me to continuously refine my
+            skills, ensuring that I deliver high-quality solutions that meet the
+            unique requirements of each project.
           </p>
           <p>
             Feel free to explore my portfolio and discover some of the projects
