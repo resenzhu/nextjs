41b8183d56a0fc07ab6c156c1405f6d24fbf86e2
resenzhu <resen.zhu@gmail.com>
2023-08-09 14:45:24 +0700

    add offset prop

--- src/components/main/shared/lazyload.tsx
+++ src/components/main/shared/lazyload.tsx
@@ -4,11 +4,12 @@ import type {ReactNode} from 'react';
 import SharedLazyLoad from 'react-lazy-load';
 
 type LazyLoadProps = {
+  offset?: number;
   children: ReactNode;
 };
 
-const LazyLoad = ({children}: LazyLoadProps): JSX.Element => (
-  <SharedLazyLoad>{children}</SharedLazyLoad>
+const LazyLoad = ({offset = -150, children}: LazyLoadProps): JSX.Element => (
+  <SharedLazyLoad offset={offset}>{children}</SharedLazyLoad>
 );
 
 export type {LazyLoadProps};
