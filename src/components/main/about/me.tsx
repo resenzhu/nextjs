90223c8ea5d7761e35325c6db066651b4c0d9d57
resenzhu <resen.zhu@gmail.com>
2023-08-09 14:55:27 +0700

    lazy load animation

--- src/components/main/about/me.tsx
+++ src/components/main/about/me.tsx
@@ -12,35 +12,41 @@ const Me = ({title, yoe}: MeProps): JSX.Element => (
         {title}
       </div>
     </LazyLoad>
-    <LazyLoad>
-      <div className='flex-1 animate-fade-up space-y-6 text-center text-gray-700 animate-duration-700'>
-        <p>
-          Welcome to my personal portfolio website! My name is Resen, and I&#39;m
-          a Full Stack developer based in Jakarta with over {yoe} years of
-          experience in the field.
+    <div className='flex-1 space-y-6 text-center text-gray-700'>
+      <LazyLoad>
+        <p className='animate-fade-right animate-duration-700'>
+          Welcome to my personal portfolio website! My name is Resen, and
+          I&#39;m a Full Stack developer based in Jakarta with over {yoe} years
+          of experience in the field.
         </p>
-        <p>
+      </LazyLoad>
+      <LazyLoad>
+        <p className='animate-fade-left animate-duration-700'>
           I specialize in both Front End and Back End development, bringing a
           comprehensive skill set to the table. With a strong focus on creating
           visually appealing and user-friendly web applications, I utilize
-          technologies such as ReactJS, NextJS, and NodeJS to deliver exceptional
-          results.
+          technologies such as ReactJS, NextJS, and NodeJS to deliver
+          exceptional results.
         </p>
-        <p>
+      </LazyLoad>
+      <LazyLoad>
+        <p className='animate-fade-right animate-duration-700'>
           From crafting captivating user interfaces to implementing robust
           functionality, I strive to create seamless and efficient websites. My
           passion for development drives me to continuously refine my skills,
           ensuring that I deliver high-quality solutions that meet the unique
           requirements of each project.
         </p>
-        <p>
+      </LazyLoad>
+      <LazyLoad>
+        <p className='animate-fade-left animate-duration-700'>
           Feel free to explore my portfolio and discover some of the projects I
-          have worked on. If you have any questions, please don&#39;t hesitate to
-          reach out. I am always open to connecting with anyone who shares a
+          have worked on. If you have any questions, please don&#39;t hesitate
+          to reach out. I am always open to connecting with anyone who shares a
           passion for web development and innovation.
         </p>
-      </div>
-    </LazyLoad>
+      </LazyLoad>
+    </div>
   </div>
 );
 
