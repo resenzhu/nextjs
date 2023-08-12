36beb80744768d2dbcf444b1b5bb96633ae07196
Resen <resen.zhu@gmail.com>
2023-08-12 22:35:53 +0700

    remove about skill component

--- src/components/main/about/index.tsx
+++ src/components/main/about/index.tsx
@@ -1,3 +1,2 @@
 export {default as Experience} from '@components/main/about/experience';
 export {default as Me} from '@components/main/about/me';
-export {default as Skill} from '@components/main/about/skill';
