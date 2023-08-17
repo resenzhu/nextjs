164bf1e0074703b9a908c545c8c9be87d60457f1
Resen <resen.zhu@gmail.com>
2023-08-17 15:38:48 +0700

    add tooltip component

--- src/components/main/shared/index.tsx
+++ src/components/main/shared/index.tsx
@@ -1,3 +1,4 @@
 export {default as Footer} from '@components/main/shared/footer';
 export {default as Header} from '@components/main/shared/header';
 export {default as LazyLoad} from '@components/main/shared/lazyload';
+export {default as Tooltip} from '@components/main/shared/tooltip';
---
+++ src/components/main/shared/tooltip.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import type {ReactNode} from 'react';
+import {Tooltip as ReactTooltip} from 'react-tooltip';
+
+type TooltipProps = {
+  id: string;
+  children: ReactNode;
+};
+
+const Tooltip = ({id, children}: TooltipProps): JSX.Element => (
+  <>
+    {children}
+    <ReactTooltip
+      className='rounded-lg bg-cyan-700 px-3 py-1 text-white'
+      id={id}
+      opacity={1}
+      disableStyleInjection={true}
+    />
+  </>
+);
+
+export type {TooltipProps};
+export default Tooltip;
