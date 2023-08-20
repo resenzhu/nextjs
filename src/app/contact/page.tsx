65f5c22bdd8cc81d9116010521b12ebbac545a94
Resen <resen.zhu@gmail.com>
2023-08-20 23:24:39 +0700

    add form and footer

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,5 @@
-import {Header} from '@components/main/shared';
+import {Footer, Header} from '@components/main/shared';
+import {Form} from '@components/main/contact';
 import {createMetadata} from '@utils/metadata';
 
 const metadata = createMetadata({
@@ -9,13 +10,30 @@ const metadata = createMetadata({
 });
 
 const Page = (): JSX.Element => (
-  <section>
-    <Header
-      title='contact'
-      subtitle='Explore Your Options by Getting in Touch'
-      description='Contact me to explore partnership opportunities or simply to drop a friendly message. Feel free to reach out via the contact form or connect with me on social media for updates and insights into my work.'
-    />
-  </section>
+  <>
+    <section>
+      <Header
+        title='contact'
+        subtitle='Explore Your Options by Getting in Touch'
+        description='Contact me to explore partnership opportunities or simply to drop a friendly message. Feel free to reach out via the contact form or connect with me on social media for updates and insights into my work.'
+      />
+    </section>
+    <section className='h-full min-h-[40vh]'>
+      <Form
+        title='STAY CONNECTED'
+        description="I'd love to hear from you! Whether you have a question, feedback, or just want to chat, feel free to reach out using the contact form below. Let's stay connected!"
+        label={{
+          name: 'Name',
+          email: 'Email',
+          message: 'Message',
+          submit: 'SUBMIT'
+        }}
+      />
+    </section>
+    <section className='h-full min-h-[12rem] pt-20'>
+      <Footer />
+    </section>
+  </>
 );
 
 export {metadata};
