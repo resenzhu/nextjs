ced933503542584bd60bab3067789406c894221e
resenzhu <resen.zhu@gmail.com>
2023-08-21 16:41:00 +0700

    update input

--- src/components/main/contact/form/input.tsx
+++ src/components/main/contact/form/input.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import {type ChangeEvent, type FormEvent, useState} from 'react';
+
 type InputProps = {
   label: {
     name: string;
@@ -7,9 +11,115 @@ type InputProps = {
   };
 };
 
-const Input = ({label}: InputProps): JSX.Element => (
-  <form></form>
-);
+type Form = {
+  name: string;
+  email: string;
+  message: string;
+};
 
-export type {InputProps};
+const Input = ({label}: InputProps): JSX.Element => {
+  const [form, setForm] = useState<Form>({name: '', email: '', message: ''});
+
+  const handleUpdateForm = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void => {
+    const property = event.target.id as keyof Form;
+    const {value} = event.target;
+    if (form[property] !== value) {
+      setForm({
+        ...form,
+        [property]: value
+      });
+    }
+  };
+
+  const handleTrimForm = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void => {
+    const property = event.target.id as keyof Form;
+    const {value} = event.target;
+    if (form[property] !== value.trim()) {
+      setForm({
+        ...form,
+        [property]: value.trim()
+      });
+    }
+  };
+
+  const handleSubmitForm = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+  };
+
+  return (
+    <form
+      className='flex flex-col justify-center space-y-5'
+      onSubmit={(event): void => handleSubmitForm(event)}
+    >
+      <div className='flex flex-col text-start'>
+        {form.name.length !== 0 && (
+          <label
+            className='font-semibold'
+            htmlFor='name'
+          >
+            {label.name}
+          </label>
+        )}
+        <input
+          className='border-b-2 pb-2 pt-1 outline-0'
+          id='name'
+          type='text'
+          placeholder={label.name}
+          value={form.name}
+          onChange={(event): void => handleUpdateForm(event)}
+          onBlur={(event): void => handleTrimForm(event)}
+        />
+      </div>
+      <div className='flex flex-col text-start'>
+        {form.email.length !== 0 && (
+          <label
+            className='font-semibold'
+            htmlFor='email'
+          >
+            {label.email}
+          </label>
+        )}
+        <input
+          className='border-b-2 pb-2 pt-1 outline-0'
+          id='email'
+          type='text'
+          placeholder={label.email}
+          value={form.email}
+          onChange={(event): void => handleUpdateForm(event)}
+          onBlur={(event): void => handleTrimForm(event)}
+        />
+      </div>
+      <div className='flex flex-col text-start'>
+        {form.message.length !== 0 && (
+          <label
+            className='font-semibold'
+            htmlFor='message'
+          >
+            {label.message}
+          </label>
+        )}
+        <textarea
+          className='min-h-[20vh] resize-none border-b-2 pb-2 pt-1 outline-0'
+          id='message'
+          placeholder={label.message}
+          value={form.message}
+          onChange={(event): void => handleUpdateForm(event)}
+          onBlur={(event): void => handleTrimForm(event)}
+        />
+      </div>
+      <button
+        className='place-self-center bg-cyan-600 px-5 py-3 font-bold tracking-wider text-white duration-150 active:bg-cyan-700'
+        type='submit'
+      >
+        {label.submit}
+      </button>
+    </form>
+  );
+};
+
+export type {InputProps, Form};
 export default Input;
