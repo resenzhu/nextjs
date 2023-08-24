f1fe0044ab142f7c9e501b1ae3a1bef9d540a184
Resen <resen.zhu@gmail.com>
2023-08-24 21:46:17 +0700

    change order

--- environment.d.ts
+++ environment.d.ts
@@ -1,7 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    NEXT_PUBLIC_APP_SOCKET: string;
     NEXT_PUBLIC_APP_URL: string;
+    NEXT_PUBLIC_APP_SOCKET: string;
     NEXT_TELEMETRY_DISABLED: number;
   }
 }
