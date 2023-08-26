7e697f0760c831c88336f6cecee96dc836dcb9e1
Resen <resen.zhu@gmail.com>
2023-08-27 01:11:31 +0700

    add transition

--- src/components/main/home/chatbot/chat.tsx
+++ src/components/main/home/chatbot/chat.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import {Fragment} from 'react';
+import {Transition} from '@headlessui/react';
 import useApp from '@hooks/main/use-app';
 import useHome from '@hooks/main/use-home';
 
@@ -10,31 +12,35 @@ const Chat = (): JSX.Element => {
     <>
       {chatbot.chats.map(
         (chat, index): JSX.Element => (
-          <>
-            {chat.message && chat.message.length !== 0 && (
-              <div
-                className={
-                  chat.sender === 'bot'
-                    ? 'max-w-5/6 w-fit place-self-start bg-cyan-600 px-3 py-1 text-white md:text-sm'
-                    : 'max-w-5/6 w-fit place-self-end bg-gray-200 px-3 py-1 md:text-sm'
-                }
-                style={{
-                  wordBreak: 'break-word'
-                }}
-                key={index}
-              >
-                {chat.message}
-              </div>
-            )}
-          </>
+          <Transition
+            key={index}
+            show={chat.message.length !== 0}
+            as={Fragment}
+          >
+            <div
+              className={
+                chat.sender === 'bot'
+                  ? 'max-w-5/6 w-fit place-self-start bg-cyan-600 px-3 py-1 text-white md:text-sm'
+                  : 'max-w-5/6 w-fit place-self-end bg-gray-200 px-3 py-1 md:text-sm'
+              }
+              style={{
+                wordBreak: 'break-word'
+              }}
+            >
+              {chat.message}
+            </div>
+          </Transition>
         )
       )}
-      {!online && (
+      <Transition
+        show={!online}
+        as={Fragment}
+      >
         <div className='w-fit max-w-[80vw] place-self-start bg-cyan-600 px-3 py-1 text-white'>
           Oops! It appears that you&#39;re currently offline. Please ensure that
           you&#39;re connected to the internet and try again later.
         </div>
-      )}
+      </Transition>
     </>
   );
 };
