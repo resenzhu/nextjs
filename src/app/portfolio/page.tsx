dc4a0370ea239739f159cd87d7e839b0d9428b52
Resen <resen.zhu@gmail.com>
2023-08-27 19:11:17 +0700

    add project and footer section

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,4 +1,5 @@
-import {Header} from '@components/main/shared';
+import {Footer, Header} from '@components/main/shared';
+import {Project} from '@components/main/portfolio';
 import {createMetadata} from '@utils/metadata';
 
 const metadata = createMetadata({
@@ -9,13 +10,32 @@ const metadata = createMetadata({
 });
 
 const Page = (): JSX.Element => (
-  <section>
-    <Header
-      title='portfolio'
-      subtitle='A Showcase of Innovative Projects and Personal Endeavors'
-      description='Discover a captivating collection of my projects, where innovation meets design. Immerse yourself in an inspiring showcase of creativity and explore the limitless possibilities of my work.'
-    />
-  </section>
+  <>
+    <section>
+      <Header
+        title='portfolio'
+        subtitle='A Showcase of Innovative Projects and Personal Endeavors'
+        description='Discover a captivating collection of my projects, where innovation meets design. Immerse yourself in an inspiring showcase of creativity and explore the limitless possibilities of my work.'
+      />
+    </section>
+    <section className='h-full min-h-[40vh]'>
+      <Project
+        title='PROJECTS'
+        projects={[
+          {
+            name: 'Breezy',
+            slogan: 'Breeze Into Conversations',
+            description:
+              'Effortless communication is just a tap away with Breezy. Designed to simplify your conversations, Breezy offers a clean and intuitive platform where you can effortlessly chat with friends and build meaningful connections. Enjoy the freedom of breezy conversations that let you focus on what truly matters.',
+            url: '/project/breezy'
+          }
+        ]}
+      />
+    </section>
+    <section className='h-full min-h-[12rem] pt-20'>
+      <Footer />
+    </section>
+  </>
 );
 
 export {metadata};
