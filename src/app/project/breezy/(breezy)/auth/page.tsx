4ebda8684bdd877b7f905fa06c37be48c22d9345
resenzhu <resen.zhu@gmail.com>
2023-08-30 17:33:34 +0700

    add page metadata

---
+++ src/app/project/breezy/(breezy)/auth/page.tsx
@@ -0,0 +1,13 @@
+import {createMetadata} from '@utils/metadata';
+
+const metadata = createMetadata({
+  title: 'Breezy | Resen',
+  description:
+    'Join Breezy and unlock a world of seamless messaging with a simple login/signup, connecting you with friends and beyond.',
+  url: '/project/breezy/auth'
+});
+
+const Page = (): JSX.Element => <>AUTH</>;
+
+export {metadata};
+export default Page;
