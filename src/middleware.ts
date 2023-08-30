0e9ec27ca23c34235846c90deb1107857a6af81c
resenzhu <resen.zhu@gmail.com>
2023-08-30 16:08:21 +0700

    use env variable url

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,9 @@ import {type NextRequest, NextResponse} from 'next/server';
 
 const middleware = (request: NextRequest): NextResponse => {
   if (request.nextUrl.pathname === '/project/breezy') {
-    return NextResponse.redirect(new URL('/project/breezy/auth', request.url));
+    return NextResponse.redirect(
+      new URL('/project/breezy/auth', process.env.NEXT_PUBLIC_APP_URL)
+    );
   }
   return NextResponse.next();
 };
