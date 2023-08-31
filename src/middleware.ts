5ddac224a939915d554731ee779f520865d5fb81
Resen <resen.zhu@gmail.com>
2023-08-31 20:42:43 +0700

    change url endpoint to login

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import {type NextRequest, NextResponse} from 'next/server';
 const middleware = (request: NextRequest): NextResponse => {
   if (request.nextUrl.pathname === '/project/breezy') {
     return NextResponse.redirect(
-      new URL('/project/breezy/auth', process.env.NEXT_PUBLIC_APP_URL)
+      new URL('/project/breezy/login', process.env.NEXT_PUBLIC_APP_URL)
     );
   }
   return NextResponse.next();
