3015ab01cc5861b1d4ea459683f2d0272fca9506
Resen <resen.zhu@gmail.com>
2023-09-02 18:51:51 +0700

    update recaptcha token logic

--- src/components/main/contact/form/input.tsx
+++ src/components/main/contact/form/input.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {type ChangeEvent, type FormEvent, useEffect} from 'react';
+import {type ChangeEvent, type FormEvent, useEffect, useState} from 'react';
 import {
   TError,
   TLabelEmail,
@@ -35,6 +35,7 @@ type SubmitContactFormReq = {
   email: string;
   message: string;
   honeypot: string;
+  token: string;
 };
 
 type SubmitContactFormRes = {
@@ -49,6 +50,7 @@ type SubmitContactFormRes = {
 const Input = ({label}: InputProps): JSX.Element => {
   const {online} = useApp();
   const {form, setForm} = useContact();
+  const [token, setToken] = useState<string>('');
   const {executeRecaptcha} = useGoogleReCaptcha();
 
   const handleUpdateForm = (
@@ -91,17 +93,16 @@ const Input = ({label}: InputProps): JSX.Element => {
     event.preventDefault();
     let errorMessage: string = '';
     if (!online) {
-      errorMessage = 'You are currently offline. Please check your internet connection and try again later.';
+      errorMessage =
+        'You are currently offline. Please check your internet connection and try again later.';
     }
     if (form.submitting) {
-      errorMessage = 'The form is currently being submitted. Please wait for the process to complete before submitting again.';
-    }
-    if (!executeRecaptcha) {
-      errorMessage = 'Apologies, but the CAPTCHA verification is not ready yet. Please wait a moment and try again.';
+      errorMessage =
+        'The form is currently being submitted. Please wait for the process to complete before submitting again.';
     }
     setForm({
       ...form,
-      submitting: online && !form.submitting && executeRecaptcha !== undefined,
+      submitting: online && !form.submitting,
       error: errorMessage,
       success: ''
     });
@@ -109,155 +110,188 @@ const Input = ({label}: InputProps): JSX.Element => {
 
   useEffect((): void => {
     if (form.submitting) {
-      const requestSchema = object().shape({
-        name: string()
-          .ensure()
-          .required('Please enter your name.')
-          .min(2, 'Your name is too short. Please enter at least 2 characters.')
-          .max(
-            120,
-            'Your name is too long. Please enter a maximum of 120 characters.'
-          ),
-        email: string()
-          .ensure()
-          .required('Please enter your email address.')
-          .min(
-            3,
-            'Your email is too short. Please enter at least 3 characters.'
-          )
-          .max(
-            320,
-            'Your email is too long. Please enter a maximum of 320 characters.'
-          ),
-        message: string()
-          .ensure()
-          .required('Please enter a message.')
-          .min(
-            15,
-            'Your message is too short. Please enter at least 15 characters.'
-          )
-          .max(
-            2000,
-            'Your message is too long. Please enter a maximum of 2000 characters.'
-          ),
-        honeypot: string()
-          .ensure()
-          .length(
-            0,
-            'Bot detection system triggered. Please ensure you are a human and not a bot.'
-          )
-      });
-      const request: SubmitContactFormReq = {
-        name: sanitize(form.name).trim(),
-        email: sanitize(form.email).trim(),
-        message: sanitize(form.message).trim(),
-        honeypot: sanitize(form.honeypot).trim()
-      };
-      requestSchema
-        .validate(request, {abortEarly: false})
-        .then((): void => {
-          if (!validator.isAlpha(request.name, 'en-US', {ignore: ' '})) {
-            throw new ValidationError(
-              'Please enter a valid name using only letters.'
-            );
-          }
-          if (!validator.isEmail(request.email)) {
-            throw new ValidationError('Please enter a valid email address.');
-          }
-          mainSocket
-            .timeout(60000)
-            .emit(
-              'submit-contact-form',
-              request,
-              (error: Error, response: SubmitContactFormRes): void => {
-                let errorMessage: string = '';
-                if (error) {
-                  errorMessage =
-                    'Form submission failed due to a server error. We apologize for the inconvenience. Please try again later.';
-                }
-                if (response && !response.success) {
-                  switch (response.error.code) {
-                    case 40001:
-                    case 4220101:
-                    case 4220102:
-                      errorMessage = 'Please enter your name.';
-                      break;
-                    case 4220103:
-                      errorMessage =
-                        'Your name is too short. Please enter at least 2 characters.';
-                      break;
-                    case 4220104:
-                      errorMessage =
-                        'Your name is too long. Please enter a maximum of 120 characters.';
-                      break;
-                    case 4220105:
-                      errorMessage =
-                        'Please enter a valid name using only letters.';
-                      break;
-                    case 40002:
-                    case 4220201:
-                    case 4220202:
-                      errorMessage = 'Please enter your email address.';
-                      break;
-                    case 4220203:
-                      errorMessage =
-                        'Your email is too short. Please enter at least 3 characters.';
-                      break;
-                    case 4220204:
-                      errorMessage =
-                        'Your email is too long. Please enter a maximum of 320 characters.';
-                      break;
-                    case 4220205:
-                      errorMessage = 'Please enter a valid email address.';
-                      break;
-                    case 40003:
-                    case 4220301:
-                    case 4220302:
-                      errorMessage = 'Please enter a message.';
-                      break;
-                    case 4220303:
-                      errorMessage =
-                        'Your message is too short. Please enter at least 15 characters.';
-                      break;
-                    case 4220304:
-                      errorMessage =
-                        'Your message is too long. Please enter a maximum of 2000 characters.';
-                      break;
-                    case 40004:
-                    case 4220401:
-                    case 4220402:
-                      errorMessage =
-                        'Bot detection system triggered. Please ensure you are a human and not a bot.';
-                      break;
-                    default:
-                      errorMessage =
-                        'Oops! There was an error processing your form submission. Please review your information and try again.';
-                      break;
-                  }
-                }
-                setForm({
-                  ...form,
-                  submitting: false,
-                  error: errorMessage,
-                  success:
-                    response && response.success
-                      ? 'Thank you! Your form has been successfully submitted.'
-                      : ''
-                });
-              }
-            );
-        })
-        .catch((error: ValidationError): void => {
-          setForm({
-            ...form,
-            submitting: false,
-            error:
-              error.errors[0] ??
-              'Oops! There was an error processing your form submission. Please review your information and try again.'
-          });
+      new Promise<string>(async (resolve): Promise<void> => {
+        let newToken: string = '';
+        if (executeRecaptcha && token.length === 0) {
+          newToken = await executeRecaptcha();
+          setToken(newToken);
+        }
+        resolve(newToken);
+      }).then((newToken: string): void => {
+        const requestSchema = object().shape({
+          name: string()
+            .ensure()
+            .required('Please enter your name.')
+            .min(
+              2,
+              'Your name is too short. Please enter at least 2 characters.'
+            )
+            .max(
+              120,
+              'Your name is too long. Please enter a maximum of 120 characters.'
+            ),
+          email: string()
+            .ensure()
+            .required('Please enter your email address.')
+            .min(
+              3,
+              'Your email is too short. Please enter at least 3 characters.'
+            )
+            .max(
+              320,
+              'Your email is too long. Please enter a maximum of 320 characters.'
+            ),
+          message: string()
+            .ensure()
+            .required('Please enter a message.')
+            .min(
+              15,
+              'Your message is too short. Please enter at least 15 characters.'
+            )
+            .max(
+              2000,
+              'Your message is too long. Please enter a maximum of 2000 characters.'
+            ),
+          honeypot: string()
+            .ensure()
+            .length(
+              0,
+              'Bot detection system triggered. Please ensure you are a human and not a bot.'
+            ),
+          token: string()
+            .ensure()
+            .required(
+              'Apologies, the reCAPTCHA verification is not ready yet. Please wait a moment and try again.'
+            )
         });
+        const request: SubmitContactFormReq = {
+          name: sanitize(form.name).trim(),
+          email: sanitize(form.email).trim(),
+          message: sanitize(form.message).trim(),
+          honeypot: sanitize(form.honeypot).trim(),
+          token: newToken.length === 0 ? token : newToken
+        };
+        requestSchema
+          .validate(request, {abortEarly: false})
+          .then((): void => {
+            if (!validator.isAlpha(request.name, 'en-US', {ignore: ' '})) {
+              throw new ValidationError(
+                'Please enter a valid name using only letters.'
+              );
+            }
+            if (!validator.isEmail(request.email)) {
+              throw new ValidationError('Please enter a valid email address.');
+            }
+            mainSocket
+              .timeout(60000)
+              .emit(
+                'submit-contact-form',
+                request,
+                (error: Error, response: SubmitContactFormRes): void => {
+                  let errorMessage: string = '';
+                  if (error) {
+                    errorMessage =
+                      'Form submission failed due to a server error. We apologize for the inconvenience. Please try again later.';
+                  }
+                  if (response && !response.success) {
+                    switch (response.error.code) {
+                      case 40001:
+                      case 4220101:
+                      case 4220102:
+                        errorMessage = 'Please enter your name.';
+                        break;
+                      case 4220103:
+                        errorMessage =
+                          'Your name is too short. Please enter at least 2 characters.';
+                        break;
+                      case 4220104:
+                        errorMessage =
+                          'Your name is too long. Please enter a maximum of 120 characters.';
+                        break;
+                      case 4220105:
+                        errorMessage =
+                          'Please enter a valid name using only letters.';
+                        break;
+                      case 40002:
+                      case 4220201:
+                      case 4220202:
+                        errorMessage = 'Please enter your email address.';
+                        break;
+                      case 4220203:
+                        errorMessage =
+                          'Your email is too short. Please enter at least 3 characters.';
+                        break;
+                      case 4220204:
+                        errorMessage =
+                          'Your email is too long. Please enter a maximum of 320 characters.';
+                        break;
+                      case 4220205:
+                        errorMessage = 'Please enter a valid email address.';
+                        break;
+                      case 40003:
+                      case 4220301:
+                      case 4220302:
+                        errorMessage = 'Please enter a message.';
+                        break;
+                      case 4220303:
+                        errorMessage =
+                          'Your message is too short. Please enter at least 15 characters.';
+                        break;
+                      case 4220304:
+                        errorMessage =
+                          'Your message is too long. Please enter a maximum of 2000 characters.';
+                        break;
+                      case 40004:
+                      case 4220401:
+                      case 4220402:
+                        errorMessage =
+                          'Bot detection system triggered. Please ensure you are a human and not a bot.';
+                        break;
+                      default:
+                        errorMessage =
+                          'Oops! There was an error processing your form submission. Please review your information and try again.';
+                        break;
+                    }
+                  }
+                  setForm({
+                    ...form,
+                    submitting: false,
+                    error: errorMessage,
+                    success:
+                      response && response.success
+                        ? 'Thank you! Your form has been successfully submitted.'
+                        : ''
+                  });
+                  setToken('');
+                }
+              );
+          })
+          .catch((error: ValidationError): void => {
+            setForm({
+              ...form,
+              submitting: false,
+              error:
+                error.errors[0] ??
+                'Oops! There was an error processing your form submission. Please review your information and try again.'
+            });
+          });
+      });
     }
   }, [form.submitting]);
+
+  useEffect((): (() => void) => {
+    let timer: ReturnType<typeof setTimeout> | null = null;
+    if (token.length !== 0) {
+      timer = setTimeout((): void => {
+        setToken('');
+      }, 90000);
+    }
+    return (): void => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [token]);
 
   return (
     <form
