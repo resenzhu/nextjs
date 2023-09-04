32f24c6afa5d4d1dc48f955f8f2ce536264678cd
resenzhu <resen.zhu@gmail.com>
2023-09-04 14:43:44 +0700

    add recaptcha v2 and v3 variables.

--- environment.d.ts
+++ environment.d.ts
@@ -2,7 +2,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_APP_URL: string;
     NEXT_PUBLIC_APP_SOCKET: string;
-    NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY: string;
+    NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY_V2_CHECKBOX: string;
+    NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY_V3: string;
     NEXT_TELEMETRY_DISABLED: number;
   }
 }
