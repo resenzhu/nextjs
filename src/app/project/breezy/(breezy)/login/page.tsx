a7c04a6d3999be6f0ac4c670c12cb0f5392b6ced
resenzhu <resen.zhu@gmail.com>
2023-09-05 15:46:16 +0700

    update login ui

--- src/app/project/breezy/(breezy)/login/page.tsx
+++ src/app/project/breezy/(breezy)/login/page.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
 import Recaptcha from 'react-google-recaptcha';
 import {createMetadata} from '@utils/metadata';
+import {faEye} from '@fortawesome/free-solid-svg-icons';
 
 const metadata = createMetadata({
   title: 'Breezy | Resen',
@@ -13,7 +16,7 @@ const metadata = createMetadata({
 const Page = (): JSX.Element => (
   <>
     <div className='absolute h-full w-full bg-[url("/images/project/breezy/login-background.webp")] bg-cover bg-center bg-no-repeat'></div>
-    <div className='absolute h-full w-full bg-black opacity-60'></div>
+    <div className='absolute h-full w-full bg-black opacity-50'></div>
     <div>
       <div className='fixed top-0 w-full py-5 text-center font-semibold text-gray-100'>
         Welcome to Breezy
@@ -26,24 +29,49 @@ const Page = (): JSX.Element => (
               Instantly access your account and start chatting in seconds.
             </div>
           </div>
-          <div className='bg-white'>
-            <form className='mx-4 flex flex-col'>
+          <div className='max-h-[calc(70vh-4rem)] bg-white'>
+            <form className='mx-4 flex flex-col space-y-3 py-6'>
               <input
+                className='border-2 px-3 py-2 outline-0 disabled:bg-gray-100'
                 type='text'
                 placeholder='Username'
               />
-              <input
-                type='password'
-                placeholder='Password'
-              />
+              <div className='flex'>
+                <input
+                  className='flex-1 border-2 px-3 py-2 outline-0 disabled:bg-gray-100'
+                  type='password'
+                  placeholder='Password'
+                />
+                <FontAwesomeIcon
+                  className='bg-purple-500 p-4 text-xl text-white'
+                  icon={faEye}
+                />
+              </div>
               <Recaptcha
+                className='place-self-center'
                 sitekey={
                   process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY_V2_CHECKBOX
                 }
               />
-              <button type='submit'>Login</button>
-              <div>Don&#39;t have an account? Sign up here!</div>
+              <button
+                className='bg-purple-500 py-2 text-lg font-semibold tracking-wide text-white duration-150 hover:bg-purple-600 disabled:bg-gray-300'
+                type='submit'
+              >
+                Login
+              </button>
+              <div className='break-words bg-red-500 px-3 py-2 text-center text-white'>
+                Username is already taken.
+              </div>
             </form>
+            <div className='mx-4 pb-4 text-center'>
+              Don&#39;t have an account?{' '}
+              <Link
+                className='font-bold text-purple-500 hover:underline hover:underline-offset-4'
+                href='/project/breezy/signup'
+              >
+                Sign up here!
+              </Link>
+            </div>
           </div>
         </div>
       </div>
