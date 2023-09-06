e70e29987b184e45e35da2d3a2006f2b6742ab7a
resenzhu <resen.zhu@gmail.com>
2023-09-06 14:28:36 +0700

    update chat styles

--- src/app/project/breezy/(breezy)/page.tsx
+++ src/app/project/breezy/(breezy)/page.tsx
@@ -1,7 +1,7 @@
+import {faCheck, faCheckDouble} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import {createMetadata} from '@utils/metadata';
-import {faCheckDouble} from '@fortawesome/free-solid-svg-icons';
 
 const metadata = createMetadata({
   title: 'Breezy | Resen',
@@ -13,15 +13,15 @@ const metadata = createMetadata({
 const Page = (): JSX.Element => (
   <div className='flex flex-col'>
     <div className='flex w-screen border-b-2 text-lg font-bold shadow-sm'>
-      <div className='flex-1 border-b-4 border-b-purple-500 py-3 text-center tracking-wide text-purple-500'>
+      <div className='flex-1 cursor-pointer border-b-4 border-b-purple-500 py-3 text-center tracking-wide text-purple-500'>
         CHATS
       </div>
-      <div className='flex-1 py-3 text-center tracking-wide text-gray-500'>
+      <div className='flex-1 cursor-pointer py-3 text-center tracking-wide text-gray-500'>
         USERS
       </div>
     </div>
-    <div className='flex-1 px-5 py-3'>
-      <div className='flex items-center'>
+    <div className='flex-1'>
+      <div className='flex cursor-pointer items-center px-5 py-3 hover:bg-gray-100'>
         <div className='mr-4'>
           <Image
             className='rounded-full'
@@ -32,17 +32,95 @@ const Page = (): JSX.Element => (
           />
         </div>
         <div className='flex flex-1 flex-col'>
-          <div className='flex justify-between'>
+          <div className='flex items-center justify-between'>
             <div className='text-lg font-bold text-gray-700'>Resen</div>
             <div className='text-gray-500'>Wednesday</div>
           </div>
           <div className='flex justify-between'>
             <p className='leading-5 text-gray-500'>
-              Batman I thought you were at home, but your house is not aaa...
+              Batman I thought you were at home, but your house is not there
             </p>
             <FontAwesomeIcon
-              className='ml-2 text-lg text-green-500'
+              className='ml-2 mt-1 text-lg text-green-500'
               icon={faCheckDouble}
+            />
+          </div>
+        </div>
+      </div>
+      <div className='flex cursor-pointer items-center px-5 py-3 hover:bg-gray-100'>
+        <div className='mr-4'>
+          <Image
+            className='rounded-full'
+            src='/images/project/breezy/chat-profile-picture.webp'
+            width={70}
+            height={70}
+            alt='profile picture'
+          />
+        </div>
+        <div className='flex flex-1 flex-col'>
+          <div className='flex items-center justify-between'>
+            <div className='text-lg font-bold text-gray-700'>
+              Bruce Wayne Brucee
+            </div>
+            <div className='font-bold text-purple-500'>12:30</div>
+          </div>
+          <div className='flex justify-between'>
+            <p className='inline-block w-full text-ellipsis leading-5 text-gray-500'>
+              Batman I thought you were at home, but your house is...
+            </p>
+            <div className='h-fit w-fit rounded-full bg-purple-500 px-2 font-bold text-white'>
+              999+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='flex cursor-pointer items-center px-5 py-3 hover:bg-gray-100'>
+        <div className='mr-4'>
+          <Image
+            className='rounded-full'
+            src='/images/project/breezy/chat-profile-picture.webp'
+            width={70}
+            height={70}
+            alt='profile picture'
+          />
+        </div>
+        <div className='flex flex-1 flex-col'>
+          <div className='flex items-center justify-between'>
+            <div className='text-lg font-bold text-gray-700'>Batman</div>
+            <div className='font-bold text-purple-500'>12:30</div>
+          </div>
+          <div className='flex justify-between'>
+            <p className='inline-block w-full text-ellipsis font-semibold leading-5 text-purple-500'>
+              Typing...
+            </p>
+            <div className='h-fit w-fit rounded-full bg-purple-500 px-2 font-bold text-white'>
+              2
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='flex cursor-pointer items-center px-5 py-3 hover:bg-gray-100'>
+        <div className='mr-4'>
+          <Image
+            className='rounded-full'
+            src='/images/project/breezy/chat-profile-picture.webp'
+            width={70}
+            height={70}
+            alt='profile picture'
+          />
+        </div>
+        <div className='flex flex-1 flex-col'>
+          <div className='flex items-center justify-between'>
+            <div className='text-lg font-bold text-gray-700'>Scarecrow</div>
+            <div className='text-gray-500'>19:42</div>
+          </div>
+          <div className='flex justify-between'>
+            <p className='leading-5 text-gray-500'>
+              Batman I thought you were at home, but your house is not there
+            </p>
+            <FontAwesomeIcon
+              className='ml-2 mt-1 text-lg text-gray-400'
+              icon={faCheck}
             />
           </div>
         </div>
