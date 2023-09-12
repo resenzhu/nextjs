29e19746a0f9a19b74832c16d74d679cb7d31565
resenzhu <resen.zhu@gmail.com>
2023-09-12 16:37:43 +0700

    implement auth and form components

--- src/app/project/breezy/(breezy)/login/page.tsx
+++ src/app/project/breezy/(breezy)/login/page.tsx
@@ -1,10 +1,7 @@
-'use client';
-
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {Auth} from '@components/project/breezy/shared';
+import {Form} from '@components/project/breezy/login';
 import Link from 'next/link';
-import Recaptcha from 'react-google-recaptcha';
 import {createMetadata} from '@utils/metadata';
-import {faEye} from '@fortawesome/free-solid-svg-icons';
 
 const metadata = createMetadata({
   title: 'Breezy | Resen',
@@ -14,65 +11,25 @@ const metadata = createMetadata({
 });
 
 const Page = (): JSX.Element => (
-  <div>
-    <div className='absolute h-full w-full bg-[url("/images/project/breezy/login-background.webp")] bg-cover bg-center bg-no-repeat'></div>
-    <div className='absolute h-full w-full bg-black opacity-60'></div>
-    <div className='relative flex h-screen flex-col justify-between'>
-      <div className='mx-4 py-5 text-center font-semibold text-gray-100'>
-        Welcome to Breezy
+  <Auth
+    background='bg-[url("/images/project/breezy/login-background.webp")]'
+    welcome='Welcome to Breezy'
+    title='Login'
+    subtitle='Instantly access your account and start chatting in seconds.'
+    footer={
+      <div className='mx-4 pb-4 text-center'>
+        Don&#39;t have an account?{' '}
+        <Link
+          className='font-bold text-purple-500 hover:underline hover:underline-offset-4'
+          href='/project/breezy/signup'
+        >
+          Sign up here!
+        </Link>
       </div>
-      <div className='space-y-9'>
-        <div className='mx-4 space-y-4'>
-          <div className='text-4xl font-semibold text-white'>Login</div>
-          <div className='tracking-wide text-gray-400'>
-            Instantly access your account and start chatting in seconds.
-          </div>
-        </div>
-        <div className='bg-white'>
-          <form className='mx-4 flex flex-col space-y-3 py-6'>
-            <input
-              className='rounded-lg border-2 px-3 py-2 outline-0 disabled:bg-gray-100'
-              type='text'
-              placeholder='Username'
-            />
-            <div className='flex'>
-              <input
-                className='flex-1 rounded-l-lg border-2 px-3 py-2 outline-0 disabled:bg-gray-100'
-                type='password'
-                placeholder='Password'
-              />
-              <FontAwesomeIcon
-                className='rounded-r-lg bg-purple-500 p-4 text-xl text-white'
-                icon={faEye}
-              />
-            </div>
-            <Recaptcha
-              className='place-self-center'
-              sitekey={process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY_V2_CHECKBOX}
-            />
-            <button
-              className='rounded-lg bg-purple-500 py-2 text-lg font-semibold tracking-wide text-white duration-150 hover:bg-purple-600 disabled:bg-gray-300'
-              type='submit'
-            >
-              Login
-            </button>
-            <div className='break-words rounded-lg bg-red-500 py-2 text-center text-white'>
-              Username is already taken.
-            </div>
-          </form>
-          <div className='mx-4 pb-4 text-center'>
-            Don&#39;t have an account?{' '}
-            <Link
-              className='font-bold text-purple-500 hover:underline hover:underline-offset-4'
-              href='/project/breezy/signup'
-            >
-              Sign up here!
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+    }
+  >
+    <Form label={{username: 'Username', password: 'Password', submit: 'Login'}} />
+  </Auth>
 );
 
 export {metadata};
--- src/app/project/breezy/(breezy)/signup/page.tsx
+++ src/app/project/breezy/(breezy)/signup/page.tsx
@@ -1,10 +1,7 @@
-'use client';
-
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {Auth} from '@components/project/breezy/shared';
+import {Form} from '@components/project/breezy/signup';
 import Link from 'next/link';
-import Recaptcha from 'react-google-recaptcha';
 import {createMetadata} from '@utils/metadata';
-import {faEye} from '@fortawesome/free-solid-svg-icons';
 
 const metadata = createMetadata({
   title: 'Breezy | Resen',
@@ -14,70 +11,25 @@ const metadata = createMetadata({
 });
 
 const Page = (): JSX.Element => (
-  <div>
-    <div className='absolute h-full w-full bg-[url("/images/project/breezy/login-background.webp")] bg-cover bg-center bg-no-repeat'></div>
-    <div className='absolute h-full w-full bg-black opacity-60'></div>
-    <div className='relative flex h-screen flex-col justify-between'>
-      <div className='mx-4 py-5 text-center font-semibold text-gray-100'>
-        Welcome to Breezy
+  <Auth
+    background='bg-[url("/images/project/breezy/login-background.webp")]'
+    welcome='Welcome to Breezy'
+    title='Sign Up'
+    subtitle='Join Breezy and start connecting with others in no time.'
+    footer={
+      <div className='mx-4 pb-4 text-center'>
+        Already have an account?{' '}
+        <Link
+        className='font-bold text-purple-500 hover:underline hover:underline-offset-4'
+        href='/project/breezy/signup'
+      >
+        Login here!
+      </Link>
       </div>
-      <div className='space-y-9'>
-        <div className='mx-4 space-y-4'>
-          <div className='text-4xl font-semibold text-white'>Sign Up</div>
-          <div className='tracking-wide text-gray-400'>
-            Join Breezy and start connecting with others in no time.
-          </div>
-        </div>
-        <div className='bg-white'>
-          <form className='mx-4 flex flex-col space-y-3 py-6'>
-            <input
-              className='rounded-lg border-2 px-3 py-2 outline-0 disabled:bg-gray-100'
-              type='text'
-              placeholder='Username'
-            />
-            <input
-              className='rounded-lg border-2 px-3 py-2 outline-0 disabled:bg-gray-100'
-              type='text'
-              placeholder='Display Name'
-            />
-            <div className='flex'>
-              <input
-                className='flex-1 rounded-l-lg border-2 px-3 py-2 outline-0 disabled:bg-gray-100'
-                type='password'
-                placeholder='Password'
-              />
-              <FontAwesomeIcon
-                className='rounded-r-lg bg-purple-500 p-4 text-xl text-white'
-                icon={faEye}
-              />
-            </div>
-            <Recaptcha
-              className='place-self-center'
-              sitekey={process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY_V2_CHECKBOX}
-            />
-            <button
-              className='rounded-lg bg-purple-500 py-2 text-lg font-semibold tracking-wide text-white duration-150 hover:bg-purple-600 disabled:bg-gray-300'
-              type='submit'
-            >
-              Sign Up
-            </button>
-            <div className='break-words rounded-lg bg-red-500 py-2 text-center text-white'>
-              Username is already taken.
-            </div>
-          </form>
-          <div className='mx-4 pb-4 text-center'>
-            Already have an account?{' '}
-            <Link
-              className='font-bold text-purple-500 hover:underline hover:underline-offset-4'
-              href='/project/breezy/signup'
-            >
-              Login here!
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+    }
+  >
+    <Form label={{username: 'Username', displayName: 'Display Name', password: 'Password', submit: 'Login'}} />
+  </Auth>
 );
 
 export {metadata};
