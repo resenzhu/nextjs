fbd3f251cfe19f02947178f986e13ac3de51ddbc
Resen <resen.zhu@gmail.com>
2023-09-12 20:35:54 +0700

    remove background and footer props

--- src/components/project/breezy/shared/auth.tsx
+++ src/components/project/breezy/shared/auth.tsx
@@ -1,26 +1,15 @@
 import type {ReactNode} from 'react';
 
 type AuthProps = {
-  background: string;
   welcome: string;
   title: string;
   subtitle: string;
-  footer: ReactNode;
   children: ReactNode;
 };
 
-const Auth = ({
-  background,
-  welcome,
-  title,
-  subtitle,
-  footer,
-  children
-}: AuthProps): JSX.Element => (
+const Auth = ({welcome, title, subtitle, children}: AuthProps): JSX.Element => (
   <>
-    <div
-      className={`absolute h-full w-full ${background} bg-cover bg-center bg-no-repeat`}
-    ></div>
+    <div className='absolute h-full w-full bg-[url("/images/project/breezy/login-background.webp")] bg-cover bg-center bg-no-repeat'></div>
     <div className='absolute h-full w-full bg-black opacity-60'></div>
     <div className='relative flex h-screen flex-col justify-between'>
       <div className='mx-4 py-5 text-center font-semibold text-gray-100'>
@@ -31,10 +20,7 @@ const Auth = ({
           <div className='text-4xl font-semibold text-white'>{title}</div>
           <div className='tracking-wide text-gray-400'>{subtitle}</div>
         </div>
-        <div className='bg-white'>
-          {children}
-          {footer}
-        </div>
+        <div className='bg-white'>{children}</div>
       </div>
     </div>
   </>
