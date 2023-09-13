220091c7ebc272451245d311160aafe6ad5fd275
Resen <resen.zhu@gmail.com>
2023-09-13 22:13:28 +0700

    remove md styles

--- src/components/project/breezy/home/navbar.tsx
+++ src/components/project/breezy/home/navbar.tsx
@@ -5,19 +5,19 @@ const Navbar = (): JSX.Element => (
   <div className='fixed bottom-0 flex w-screen justify-evenly border-t-2 bg-white shadow-sm'>
     <div className='grid flex-1 place-content-center border-t-4 border-t-purple-500 py-4 text-purple-500 duration-150'>
       <FontAwesomeIcon
-        className='cursor-pointer text-xl md:text-lg'
+        className='cursor-pointer text-xl'
         icon={faMessage}
       />
     </div>
-    <div className='grid flex-1 place-content-center py-4 text-gray-500 duration-150 md:flex-none'>
+    <div className='grid flex-1 place-content-center py-4 text-gray-500 duration-150'>
       <FontAwesomeIcon
-        className='cursor-pointer text-xl hover:text-purple-500 md:text-lg'
+        className='cursor-pointer text-xl hover:text-purple-500'
         icon={faUserFriends}
       />
     </div>
-    <div className='grid flex-1 place-content-center py-4 text-gray-500 duration-150 md:flex-none'>
+    <div className='grid flex-1 place-content-center py-4 text-gray-500 duration-150'>
       <FontAwesomeIcon
-        className='cursor-pointer text-2xl hover:text-purple-500 md:text-xl'
+        className='cursor-pointer text-2xl hover:text-purple-500'
         icon={faUserCircle}
       />
     </div>
