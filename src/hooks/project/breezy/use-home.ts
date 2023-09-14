1dd95d26589b9ccdbee1a81cfb2ba0e5cb99aa0c
Resen <resen.zhu@gmail.com>
2023-09-14 18:42:02 +0700

    add breezy home menu hook

---
+++ src/hooks/project/breezy/use-home.ts
@@ -0,0 +1,26 @@
+import {
+  type Menu,
+  setMenu as setHomeMenu
+} from '@redux/reducers/project/breezy/home';
+import {useDispatch, useSelector} from '@redux/hooks';
+
+type UseHome = {
+  menu: Menu;
+  setMenu: (menu: Menu) => void;
+};
+
+const useHome = (): UseHome => {
+  const dispatch = useDispatch();
+
+  const setMenu = (menu: Menu): void => {
+    dispatch(setHomeMenu(menu));
+  };
+
+  return {
+    menu: useSelector((state) => state.project.breezy.menu),
+    setMenu: setMenu
+  };
+};
+
+export type {UseHome};
+export default useHome;
