a590bf260d5da3b656852ee5426d9fb7c7fcb245
Resen <resen.zhu@gmail.com>
2023-09-16 16:40:54 +0700

    add breezy signup state

---
+++ src/redux/reducers/project/breezy/signup.ts
@@ -0,0 +1,55 @@
+import {type PayloadAction, createSlice} from '@reduxjs/toolkit';
+
+type Form = {
+  username: string;
+  displayName: string;
+  password: string;
+  honeypot: string;
+  token: string;
+  throttle: boolean;
+  submitting: boolean;
+  error: string;
+  success: string;
+};
+
+type State = {
+  form: Form;
+};
+
+type Reducers = {
+  setForm: (state: State, action: PayloadAction<Form>) => void;
+};
+
+const name: string = 'signup';
+
+const initialState: State = {
+  form: {
+    username: '',
+    displayName: '',
+    password: '',
+    honeypot: '',
+    token: '',
+    throttle: true,
+    submitting: false,
+    error: '',
+    success: ''
+  }
+};
+
+const reducers: Reducers = {
+  setForm: (state, action) => {
+    if (action.payload !== state.form) {
+      state.form = action.payload;
+    }
+  }
+};
+
+const slice = createSlice({
+  name: name,
+  initialState: initialState,
+  reducers: reducers
+});
+
+export type {Form};
+export const {setForm} = slice.actions;
+export default slice.reducer;
