d82298628300861766c04fc9cc2d80e5b515e663
Resen <resen.zhu@gmail.com>
2023-09-16 23:51:24 +0700

    add signup reducer

--- src/redux/reducers/project/breezy/index.ts
+++ src/redux/reducers/project/breezy/index.ts
@@ -2,8 +2,10 @@ import {combineReducers} from '@reduxjs/toolkit';
 import {createPersistConfig} from '@utils/persist';
 import homeReducer from '@redux/reducers/project/breezy/home';
 import {persistReducer} from 'redux-persist';
+import signUpReducer from '@redux/reducers/project/breezy/signup';
 
 const combinedReducers = combineReducers({
+  signup: persistReducer(createPersistConfig({key: 'signup'}), signUpReducer),
   home: persistReducer(createPersistConfig({key: 'home'}), homeReducer)
 });
 
