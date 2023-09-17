08b61ecfff7447e2c18c6af7589861a53cb43c65
Resen <resen.zhu@gmail.com>
2023-09-17 09:10:36 +0700

    add signup error transition

---
+++ src/components/project/breezy/signup/form/transition.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import {Fragment, type ReactNode} from 'react';
+import {Transition} from '@headlessui/react';
+import useSignUp from '@hooks/project/breezy/use-signup';
+
+export type TProps = {
+  children: ReactNode;
+};
+
+export const TError = ({children}: TProps): JSX.Element => {
+  const {form} = useSignUp();
+  return (
+    <Transition
+      show={form.error.length !== 0}
+      as={Fragment}
+    >
+      {children}
+    </Transition>
+  );
+};
