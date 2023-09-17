9c464e4710e3563c120943f1e6392d2ee9546a90
Resen <resen.zhu@gmail.com>
2023-09-17 16:38:31 +0700

    disable auto connect

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -2,7 +2,8 @@ import {Manager} from 'socket.io-client';
 
 const socketManager = new Manager(process.env.NEXT_PUBLIC_APP_SOCKET, {
   transports: ['websocket', 'polling'],
-  rejectUnauthorized: process.env.NODE_ENV === 'production'
+  rejectUnauthorized: process.env.NODE_ENV === 'production',
+  autoConnect: false
 });
 
 export const mainSocket = socketManager.socket('/main');
