a6236994805e5f27a8d74344cf32c33d1a272914
Resen <resen.zhu@gmail.com>
2023-09-17 17:13:43 +0700

    implement socket

--- src/app/project/breezy/(breezy)/layout.tsx
+++ src/app/project/breezy/(breezy)/layout.tsx
@@ -1,12 +1,13 @@
 import '@app/project/breezy/(breezy)/layout.css';
 import type {ReactNode} from 'react';
+import {Socket} from '@components/project/breezy/shared';
 
 type RootLayoutProps = {
   children: ReactNode;
 };
 
 const RootLayout = ({children}: RootLayoutProps): JSX.Element => (
-  <>{children}</>
+  <Socket>{children}</Socket>
 );
 
 export type {RootLayoutProps};
