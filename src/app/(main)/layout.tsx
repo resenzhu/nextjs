03f0cff91ad57993113304fb1b03ec717d6ba660
Resen <resen.zhu@gmail.com>
2023-09-17 17:07:16 +0700

    implement socket

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,45 +1,48 @@
 import '@app/(main)/layout.css';
 import Navbar from '@components/main/navbar';
 import type {ReactNode} from 'react';
+import {Socket} from '@components/main/shared';
 
 type RootLayoutProps = {
   children: ReactNode;
 };
 
 const RootLayout = ({children}: RootLayoutProps): JSX.Element => (
-  <div className='overflow-x-hidden pt-16 md:pt-14'>
-    <Navbar
-      logo='/images/main/navbar-brand-logo.webp'
-      title='RESEN'
-    >
-      <Navbar.Menu
-        label='Home'
-        url='/'
-      />
-      <Navbar.Menu
-        label='About'
-        url='/about'
-      />
-      <Navbar.Menu
-        label='Portfolio'
-        url='/portfolio'
-      />
-      <Navbar.Menu
-        label='Resources'
-        url='/resources'
-      />
-      <Navbar.Menu
-        label='Contact'
-        url='/contact'
-      />
-      <Navbar.Menu
-        label='GitHub'
-        url='https://github.com/resenzhu'
-        target='_blank'
-      />
-    </Navbar>
-    {children}
-  </div>
+  <Socket>
+    <div className='overflow-x-hidden pt-16 md:pt-14'>
+      <Navbar
+        logo='/images/main/navbar-brand-logo.webp'
+        title='RESEN'
+      >
+        <Navbar.Menu
+          label='Home'
+          url='/'
+        />
+        <Navbar.Menu
+          label='About'
+          url='/about'
+        />
+        <Navbar.Menu
+          label='Portfolio'
+          url='/portfolio'
+        />
+        <Navbar.Menu
+          label='Resources'
+          url='/resources'
+        />
+        <Navbar.Menu
+          label='Contact'
+          url='/contact'
+        />
+        <Navbar.Menu
+          label='GitHub'
+          url='https://github.com/resenzhu'
+          target='_blank'
+        />
+      </Navbar>
+      {children}
+    </div>
+  </Socket>
 );
 
 export type {RootLayoutProps};
