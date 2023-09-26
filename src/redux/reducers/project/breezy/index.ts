37797ac315afffa7495526384a011eb03eca61f4
resenzhu <resen.zhu@gmail.com>
2023-09-26 11:22:56 +0700

    add login state

--- src/redux/reducers/project/breezy/index.ts
+++ src/redux/reducers/project/breezy/index.ts
@@ -1,11 +1,13 @@
 import {combineReducers} from '@reduxjs/toolkit';
 import {createPersistConfig} from '@utils/persist';
 import homeReducer from '@redux/reducers/project/breezy/home';
+import loginReducer from '@redux/reducers/project/breezy/login';
 import {persistReducer} from 'redux-persist';
 import signUpReducer from '@redux/reducers/project/breezy/signup';
 
 const combinedReducers = combineReducers({
   signup: persistReducer(createPersistConfig({key: 'signup'}), signUpReducer),
+  login: persistReducer(createPersistConfig({key: 'login'}), loginReducer),
   home: persistReducer(createPersistConfig({key: 'home'}), homeReducer)
 });
 
---
+++ src/redux/reducers/project/breezy/login.ts
@@ -0,0 +1,59 @@
+import {type PayloadAction, createSlice} from '@reduxjs/toolkit';
+
+type Form = {
+  username: string;
+  password: string;
+  reveal: boolean;
+  honeypot: string;
+  token: string;
+  throttle: boolean;
+  submitting: boolean;
+  error: {
+    field: string | null;
+    message: string;
+  };
+};
+
+type State = {
+  form: Form;
+};
+
+type Reducers = {
+  setForm: (state: State, action: PayloadAction<Form>) => void;
+};
+
+const name: string = 'login';
+
+const initialState: State = {
+  form: {
+    username: '',
+    password: '',
+    reveal: false,
+    honeypot: '',
+    token: '',
+    throttle: true,
+    submitting: false,
+    error: {
+      field: null,
+      message: ''
+    }
+  }
+};
+
+const reducers: Reducers = {
+  setForm: (state, action) => {
+    if (action.payload !== state.form) {
+      state.form = action.payload;
+    }
+  }
+};
+
+const slice = createSlice({
+  name: name,
+  initialState: initialState,
+  reducers: reducers
+});
+
+export type {Form};
+export const {setForm} = slice.actions;
+export default slice.reducer;
