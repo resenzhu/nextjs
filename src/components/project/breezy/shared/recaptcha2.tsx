06c388d879e2f8ee3437828f9d79ce8bc5719524
Resen <resen.zhu@gmail.com>
2023-09-28 14:41:38 +0700

    add ref prop

--- src/components/project/breezy/shared/recaptcha2.tsx
+++ src/components/project/breezy/shared/recaptcha2.tsx
@@ -1,14 +1,17 @@
 'use client';
 
+import type {LegacyRef} from 'react';
 import Recaptcha from 'react-google-recaptcha';
 
 type RecaptchaV2Props = {
+  ref: LegacyRef<Recaptcha>;
   onChange: (token: string | null) => void;
 };
 
-const RecaptchaV2 = ({onChange}: RecaptchaV2Props): JSX.Element => (
+const RecaptchaV2 = ({ref, onChange}: RecaptchaV2Props): JSX.Element => (
   <Recaptcha
     className='place-self-center'
+    ref={ref}
     sitekey={process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY_V2_CHECKBOX}
     onChange={onChange}
   />
