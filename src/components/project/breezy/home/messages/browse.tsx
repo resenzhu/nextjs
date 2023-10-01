2548b9e5d56925176f19f1155a236e3da0e3e01e
Resen <resen.zhu@gmail.com>
2023-10-02 00:08:22 +0700

    add toggle users event

--- src/components/project/breezy/home/messages/browse.tsx
+++ src/components/project/breezy/home/messages/browse.tsx
@@ -1,17 +1,34 @@
 'use client';
 
+import useHome from '@hooks/project/breezy/use-home';
+
 type BrowseProps = {
   label: string;
 };
 
-const Browse = ({label}: BrowseProps): JSX.Element => (
-  <button
-    className='rounded-lg bg-purple-500 px-5 py-2 font-semibold text-white duration-150 hover:bg-purple-600'
-    type='button'
-  >
-    {label}
-  </button>
-);
+const Browse = ({label}: BrowseProps): JSX.Element => {
+  const {menu, setMenu} = useHome();
+
+  const handleToggleUsers = (show: boolean): void => {
+    if (menu.users !== show) {
+      setMenu({
+        ...menu,
+        messages: !show,
+        users: show
+      });
+    }
+  };
+
+  return (
+    <button
+      className='rounded-lg bg-purple-500 px-5 py-2 font-semibold text-white duration-150 hover:bg-purple-600'
+      type='button'
+      onClick={(): void => handleToggleUsers(true)}
+    >
+      {label}
+    </button>
+  );
+};
 
 export type {BrowseProps};
 export default Browse;
