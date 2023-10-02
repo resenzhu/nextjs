795e4db6852734ef02c0395742f22072511fed1f
Resen <resen.zhu@gmail.com>
2023-10-02 22:36:52 +0700

    change empty message

--- src/components/project/breezy/home/users/index.tsx
+++ src/components/project/breezy/home/users/index.tsx
@@ -1,22 +1,17 @@
 import {TUsers} from '@components/project/breezy/home/users/transition';
 
-const Users = (): JSX.Element => (
+type UsersProps = {
+  empty: {
+    message: string;
+  };
+};
+
+const Users = ({empty}: UsersProps): JSX.Element => (
   <TUsers>
-    <div className='pb-14'>
-      <div className='mx-4 py-4'>
-        <input
-          className='w-full bg-gray-100 px-3 py-2 outline-0'
-          type='text'
-          placeholder='Search user'
-        />
+    <div className='mx-4 flex h-[calc(100vh-3.5rem)] items-center justify-center'>
+      <div className='w-3/4 space-y-8 text-center'>
+        <p className='text-gray-500'>{empty.message}</p>
       </div>
-      <p className='mx-4 rounded-lg bg-purple-500 px-4 py-3 text-center text-sm text-white'>
-        Currently, there are no users online for chat. To utilize the chat
-        feature, kindly create a new account on a different browser or device
-        while staying logged in to your current session. This will allow you to
-        experience the chat functionality with the newly created account. Thank
-        you for your understanding and cooperation.
-      </p>
     </div>
   </TUsers>
 );
