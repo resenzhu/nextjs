c00ae11f58b095e23a8e43c6f03978b8bb18b5b3
Resen <resen.zhu@gmail.com>
2023-10-08 10:41:07 +0700

    add breezy auth token

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,4 +1,5 @@
 import {Manager} from 'socket.io-client';
+import cookie from 'js-cookie';
 
 const socketManager = new Manager(process.env.NEXT_PUBLIC_APP_SOCKET, {
   transports: ['websocket', 'polling'],
@@ -6,5 +7,13 @@ const socketManager = new Manager(process.env.NEXT_PUBLIC_APP_SOCKET, {
   autoConnect: false
 });
 
-export const mainSocket = socketManager.socket('/main');
-export const breezySocket = socketManager.socket('/project/breezy');
+const mainSocket = socketManager.socket('/main');
+const breezySocket = socketManager.socket('/project/breezy', {
+  auth: {
+    token: cookie.get(
+      process.env.NODE_ENV === 'production' ? '__Secure-BZ' : 'BZ'
+    )
+  }
+});
+
+export {mainSocket, breezySocket};
