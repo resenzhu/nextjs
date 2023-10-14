1349d1935dd9a8a3a9b64c4cd2b0508f1a2368f7
Resen <resen.zhu@gmail.com>
2023-10-14 10:21:57 +0700

    remove socket connect error

--- src/components/project/breezy/home/refresh.tsx
+++ src/components/project/breezy/home/refresh.tsx
@@ -1,9 +1,6 @@
 'use client';
 
 import {type ReactNode, useEffect, useState} from 'react';
-import {breezySocket} from '@utils/socket';
-import cookie from 'js-cookie';
-import {useRouter} from 'next/navigation';
 
 type RefreshProps = {
   children: ReactNode;
@@ -11,16 +8,6 @@ type RefreshProps = {
 
 const Refresh = ({children}: RefreshProps): JSX.Element => {
   const [rendered, setRendered] = useState<boolean>(false);
-  const {push} = useRouter();
-
-  const handleLogout = (socketError: Error): void => {
-    if (socketError.message === 'JsonWebTokenError') {
-      cookie.remove(process.env.NEXT_PUBLIC_APP_COOKIE_BREEZY, {
-        path: '/project/breezy'
-      });
-      push('/project/breezy/login');
-    }
-  };
 
   useEffect((): void => {
     if (!rendered) {
@@ -28,13 +15,9 @@ const Refresh = ({children}: RefreshProps): JSX.Element => {
     }
   }, []);
 
-  useEffect((): (() => void) => {
+  useEffect((): void => {
     if (rendered) {
-      breezySocket.on('connect_error', handleLogout);
     }
-    return (): void => {
-      breezySocket.off('connect_error', handleLogout);
-    };
   }, [rendered]);
 
   return <>{children}</>;
