03d0aee484fc7bf471ddd480231d8ab185ad45d7
Resen <resen.zhu@gmail.com>
2023-10-14 09:48:40 +0700

    change breezy middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,34 @@ import {importSPKI, jwtVerify} from 'jose';
 const middleware = async (nextRequest: NextRequest): Promise<NextResponse> => {
   switch (nextRequest.nextUrl.pathname) {
     case '/project/breezy/signup':
-    case '/project/breezy/login':
+    case '/project/breezy/login': {
+      const token = nextRequest.cookies.get(
+        process.env.NEXT_PUBLIC_APP_COOKIE_BREEZY
+      )?.value;
+      if (token) {
+        const jwtSpki = Buffer.from(
+          process.env.JWT_KEY_PUBLIC_BASE64,
+          'base64'
+        ).toString();
+        const jwtPublicKey = await importSPKI(jwtSpki, 'RS256');
+        try {
+          const result = await jwtVerify(token, jwtPublicKey);
+          if (result) {
+            return NextResponse.redirect(
+              new URL('/project/breezy', process.env.NEXT_PUBLIC_APP_URL)
+            );
+          }
+        } catch {
+          return NextResponse.next();
+        }
+      }
+      return NextResponse.next();
+    }
     case '/project/breezy': {
       const token = nextRequest.cookies.get(
-        process.env.NODE_ENV === 'production' ? '__Secure-BZ' : 'BZ'
+        process.env.NEXT_PUBLIC_APP_COOKIE_BREEZY
       )?.value;
-      if (!token && nextRequest.nextUrl.pathname === '/project/breezy') {
+      if (!token) {
         return NextResponse.redirect(
           new URL('/project/breezy/login', process.env.NEXT_PUBLIC_APP_URL)
         );
@@ -20,18 +42,12 @@ const middleware = async (nextRequest: NextRequest): Promise<NextResponse> => {
           'base64'
         ).toString();
         const jwtPublicKey = await importSPKI(jwtSpki, 'RS256');
-        const result = await jwtVerify(token, jwtPublicKey);
-        if (
-          result &&
-          ['/project/breezy/signup', '/project/breezy/login'].some(
-            (auth): boolean => nextRequest.nextUrl.pathname === auth
-          )
-        ) {
-          return NextResponse.redirect(
-            new URL('/project/breezy', process.env.NEXT_PUBLIC_APP_URL)
-          );
-        }
-        if (!result && nextRequest.nextUrl.pathname === '/project/breezy') {
+        try {
+          const result = await jwtVerify(token, jwtPublicKey);
+          if (result) {
+            return NextResponse.next();
+          }
+        } catch {
           return NextResponse.redirect(
             new URL('/project/breezy/login', process.env.NEXT_PUBLIC_APP_URL)
           );
