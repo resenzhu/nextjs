c1a3a04e6c5011b56476d5948e0e084ee837f15c
Resen <resen.zhu@gmail.com>
2023-10-15 11:48:17 +0700

    change handleSelectMenu logic

--- src/components/project/breezy/home/navbar/navmenu.tsx
+++ src/components/project/breezy/home/navbar/navmenu.tsx
@@ -2,7 +2,6 @@
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import type {IconDefinition} from '@fortawesome/fontawesome-svg-core';
-import type {Menu} from '@redux/reducers/project/breezy/home';
 import useHome from '@hooks/project/breezy/use-home';
 
 type NavMenuProps = {
@@ -11,34 +10,54 @@ type NavMenuProps = {
 };
 
 const NavMenu = ({name, icon}: NavMenuProps): JSX.Element => {
-  const {menu, setMenu} = useHome();
+  const {
+    messages,
+    users,
+    profile,
+    setMessages: _setMessages, // eslint-disable-line
+    setUsers: _setUsers, // eslint-disable-line
+    setProfile: _setProfile // eslint-disable-line
+  } = useHome();
   const {keys} = Object;
 
   const handleSelectMenu = (menuName: string): void => {
-    const newSelectedMenu = menuName as keyof Menu;
-    const previousMenu = keys(menu).filter(
-      (property): boolean => menu[property as keyof Menu]
-    )[0] as keyof Menu;
-    if (newSelectedMenu !== previousMenu) {
-      setMenu({
-        ...menu,
-        [newSelectedMenu]: true,
-        [previousMenu]: false
-      });
+    const menuMap = {
+      messages: messages,
+      users: users,
+      profile: profile
+    };
+    const previousMenu =
+      keys(menuMap).find(
+        (menu): boolean => menuMap[menu as keyof typeof menuMap].show
+      ) ?? 'messages';
+    const nextMenu = menuName as keyof typeof menuMap;
+    if (nextMenu !== previousMenu) {
+      // eslint-disable-next-line
+      eval(
+        `_set${previousMenu.charAt(0).toUpperCase()}${previousMenu
+          .slice(1)
+          .toLowerCase()}({...${previousMenu}, show: false})`
+      );
+      // eslint-disable-next-line
+      eval(
+        `_set${nextMenu.charAt(0).toUpperCase()}${nextMenu
+          .slice(1)
+          .toLowerCase()}({...${nextMenu}, show: true})`
+      );
     }
   };
 
   return (
     <div
       className={`grid flex-1 place-content-center border-t-4 py-4 md:flex-none md:border-r-4 md:border-t-0 md:px-3 md:py-5 ${
-        menu[name as keyof Menu]
+        eval(`${name}.show`) // eslint-disable-line
           ? 'border-t-purple-500 md:border-r-purple-500'
           : 'border-t-white md:border-r-white'
       }`}
     >
       <FontAwesomeIcon
         className={`cursor-pointer text-xl md:text-base ${
-          menu[name as keyof Menu]
+          eval(`${name}.show`) // eslint-disable-line
             ? 'text-purple-500'
             : 'text-gray-500 duration-150 hover:text-purple-500'
         }`}
