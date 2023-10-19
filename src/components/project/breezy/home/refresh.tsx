679cbf3bc02629491609619333335c3d89a7d38d
Resen <resen.zhu@gmail.com>
2023-10-19 16:44:20 +0700

    add handleLogoutOldSession

--- src/components/project/breezy/home/refresh.tsx
+++ src/components/project/breezy/home/refresh.tsx
@@ -5,6 +5,7 @@ import type {User} from '@redux/reducers/project/breezy/home';
 import {breezySocket} from '@utils/socket';
 import cookie from 'js-cookie';
 import useHome from '@hooks/project/breezy/use-home';
+import {useRouter} from 'next/navigation';
 
 type RefreshProps = {
   children: ReactNode;
@@ -42,6 +43,7 @@ type UserLoggedInNotif = {
 const Refresh = ({children}: RefreshProps): JSX.Element => {
   const [rendered, setRendered] = useState<boolean>(false);
   const {users, setUsers} = useHome();
+  const {push} = useRouter();
 
   const handleAddSignedUpUser = (signedUpUser: UserSignedUpNotif): void => {
     setUsers({
@@ -71,6 +73,13 @@ const Refresh = ({children}: RefreshProps): JSX.Element => {
     });
   };
 
+  const handleLogoutOldSession = (): void => {
+    cookie.remove(process.env.NEXT_PUBLIC_APP_COOKIE_BREEZY, {
+      path: '/project/breezy'
+    });
+    push('project/breezy/login');
+  };
+
   useEffect((): void => {
     if (!rendered) {
       setRendered(true);
@@ -81,10 +90,12 @@ const Refresh = ({children}: RefreshProps): JSX.Element => {
     if (rendered) {
       breezySocket.on('user signed up', handleAddSignedUpUser);
       breezySocket.on('user logged in', handleUpdateLoggedInUser);
+      breezySocket.on('logout old session', handleLogoutOldSession);
     }
     return (): void => {
       breezySocket.off('user signed up', handleAddSignedUpUser);
       breezySocket.off('user logged in', handleUpdateLoggedInUser);
+      breezySocket.off('logout old session', handleLogoutOldSession);
     };
   }, [rendered]);
 
