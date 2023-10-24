f183f039bb4b26895b3e63df1fbce25815145f2a
resenzhu <resen.zhu@gmail.com>
2023-10-24 10:36:57 +0700

    add home profile status component

---
+++ src/components/project/breezy/home/profile/status.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import {Listbox} from '@headlessui/react';
+import useHome from '@hooks/project/breezy/use-home';
+
+type Mode = {
+  label: string;
+  color: string;
+};
+
+type StatusProps = {
+  modes: Mode[];
+};
+
+const Status = ({modes}: StatusProps): JSX.Element => {
+  const {profile} = useHome();
+  return (
+    <Listbox>
+      <Listbox.Button
+        className={`w-40 rounded-lg bg-white py-1 text-lg font-semibold ${
+          modes.find((mode): boolean => mode.label === profile.session.status)
+            ?.color ?? 'text-green-600'
+        } hover:animate-shake hover:animate-duration-700 hover:animate-ease-in`}
+      >
+        {modes.find((mode): boolean => mode.label === profile.session.status)
+          ?.label ?? 'online'}
+      </Listbox.Button>
+      <Listbox.Options className='absolute w-40 translate-y-[8.3rem] rounded-lg bg-white text-center shadow-md'>
+        {modes.map(
+          (mode): JSX.Element => (
+            <Listbox.Option
+              className={`cursor-pointer py-2 font-semibold ${mode.color} hover:rounded-lg hover:bg-gray-100`}
+              key={mode.label}
+              value={mode.label}
+            >
+              {mode.label}
+            </Listbox.Option>
+          )
+        )}
+      </Listbox.Options>
+    </Listbox>
+  );
+};
+
+export type {Mode, StatusProps};
+export default Status;
