159f647ac4ac0319fcf1ec63a97842ff081420e5
resenzhu <resen.zhu@gmail.com>
2023-10-25 15:19:29 +0700

    add fetch animation

--- src/components/project/breezy/home/profile/transition.tsx
+++ src/components/project/breezy/home/profile/transition.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import type {ReactNode} from 'react';
+import {Fragment, type ReactNode} from 'react';
 import {Transition} from '@headlessui/react';
 import useHome from '@hooks/project/breezy/use-home';
 
@@ -11,4 +11,33 @@ export type TProps = {
 export const TProfile = ({children}: TProps): JSX.Element => {
   const {menu} = useHome();
   return <Transition show={menu.profile}>{children}</Transition>;
+};
+
+export const TFetching = ({children}: TProps): JSX.Element => {
+  const {profile} = useHome();
+  return (
+    <Transition
+      show={profile.fetching && !profile.fetched}
+      as={Fragment}
+    >
+      {children}
+    </Transition>
+  );
+};
+
+export const TRetryFetch = ({children}: TProps): JSX.Element => {
+  const {profile} = useHome();
+  return (
+    <Transition
+      show={!profile.fetching && !profile.fetched}
+      as={Fragment}
+    >
+      {children}
+    </Transition>
+  );
+};
+
+export const TFetched = ({children}: TProps): JSX.Element => {
+  const {profile} = useHome();
+  return <Transition show={profile.fetched}>{children}</Transition>;
 };
