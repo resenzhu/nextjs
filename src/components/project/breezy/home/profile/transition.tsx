df054d8ed2bbcd13c61f289d039cad90f0c99a91
Resen <resen.zhu@gmail.com>
2023-10-27 14:52:18 +0700

    add fragment to transitions

--- src/components/project/breezy/home/profile/transition.tsx
+++ src/components/project/breezy/home/profile/transition.tsx
@@ -10,7 +10,14 @@ export type TProps = {
 
 export const TProfile = ({children}: TProps): JSX.Element => {
   const {menu} = useHome();
-  return <Transition show={menu.profile}>{children}</Transition>;
+  return (
+    <Transition
+      show={menu.profile}
+      as={Fragment}
+    >
+      {children}
+    </Transition>
+  );
 };
 
 export const TFetching = ({children}: TProps): JSX.Element => {
@@ -28,7 +35,10 @@ export const TFetching = ({children}: TProps): JSX.Element => {
 export const TRetryFetch = ({children}: TProps): JSX.Element => {
   const {profile} = useHome();
   return (
-    <Transition show={!profile.fetching && !profile.fetched}>
+    <Transition
+      show={!profile.fetching && !profile.fetched}
+      as={Fragment}
+    >
       {children}
     </Transition>
   );
@@ -36,5 +46,12 @@ export const TRetryFetch = ({children}: TProps): JSX.Element => {
 
 export const TFetched = ({children}: TProps): JSX.Element => {
   const {profile} = useHome();
-  return <Transition show={profile.fetched}>{children}</Transition>;
+  return (
+    <Transition
+      show={profile.fetched}
+      as={Fragment}
+    >
+      {children}
+    </Transition>
+  );
 };
