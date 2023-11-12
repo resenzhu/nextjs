ec9fe18b378a22322e6102c192c73c892a46672f
Resen <resen.zhu@gmail.com>
2023-11-13 00:45:14 +0700

    add breezy shared modal component

---
+++ src/components/project/breezy/shared/modal.tsx
@@ -0,0 +1,9 @@
+const Modal = (): JSX.Element => {
+  return (
+    <div className='fixed'>
+      <div className='fixed left-0 top-0 h-screen w-screen bg-black opacity-60'></div>
+    </div>
+  );
+};
+
+export default Modal;
