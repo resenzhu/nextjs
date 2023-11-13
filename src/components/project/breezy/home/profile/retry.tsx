bfd0a8eae372d4984bc11ef939a8eec99f36d402
resenzhu <resen.zhu@gmail.com>
2023-11-13 16:26:01 +0700

    fix retry fetch runs twice

--- src/components/project/breezy/home/profile/retry.tsx
+++ src/components/project/breezy/home/profile/retry.tsx
@@ -1,9 +1,6 @@
 'use client';
 
-import {useEffect, useState} from 'react';
 import {Button} from '@components/project/breezy/shared';
-import type {FetchProfileRes} from '@components/project/breezy/home/refresh';
-import {breezySocket} from '@utils/socket';
 import useHome from '@hooks/project/breezy/use-home';
 
 type RetryProps = {
@@ -12,55 +9,15 @@ type RetryProps = {
 
 const Retry = ({label}: RetryProps): JSX.Element => {
   const {profile, setProfile} = useHome();
-  const [rendered, setRendered] = useState<boolean>(false);
 
   const handleRetryFetch = (): void => {
-    setProfile({
-      ...profile,
-      fetching: true
-    });
+    if (!profile.fetching && !profile.fetched) {
+      setProfile({
+        ...profile,
+        fetching: true
+      });
+    }
   };
-
-  useEffect((): void => {
-    if (!rendered) {
-      setRendered(true);
-    }
-  }, []);
-
-  useEffect((): void => {
-    if (rendered && profile.fetching && !profile.fetched) {
-      breezySocket
-        .timeout(60000)
-        .emit(
-          'fetch profile',
-          (socketError: Error, response: FetchProfileRes): void => {
-            setProfile({
-              ...profile,
-              fetching: false,
-              fetched: !socketError && response.data.user !== undefined,
-              user:
-                socketError || response.data.user === undefined
-                  ? {...profile.user}
-                  : {
-                      ...profile.user,
-                      id: response.data.user.id,
-                      username: response.data.user.username,
-                      displayName: response.data.user.displayName,
-                      session: {
-                        ...profile.user.session,
-                        status: {
-                          ...profile.user.session.status,
-                          previous: response.data.user.session.status,
-                          current: response.data.user.session.status
-                        },
-                        lastOnline: response.data.user.session.lastOnline
-                      }
-                    }
-            });
-          }
-        );
-    }
-  }, [rendered, profile]);
 
   return <Button onClick={(): void => handleRetryFetch()}>{label}</Button>;
 };
--- src/components/project/breezy/home/users/retry.tsx
+++ src/components/project/breezy/home/users/retry.tsx
@@ -1,9 +1,6 @@
 'use client';
 
-import {useEffect, useState} from 'react';
 import {Button} from '@components/project/breezy/shared';
-import type {FetchUsersRes} from '@components/project/breezy/home/refresh';
-import {breezySocket} from '@utils/socket';
 import useHome from '@hooks/project/breezy/use-home';
 
 type RetryProps = {
@@ -11,39 +8,16 @@ type RetryProps = {
 };
 
 const Retry = ({label}: RetryProps): JSX.Element => {
-  const [rendered, setRendered] = useState<boolean>(false);
   const {users, setUsers} = useHome();
 
   const handleRetryFetch = (): void => {
-    setUsers({
-      ...users,
-      fetching: true
-    });
+    if (!users.fetching && !users.fetched) {
+      setUsers({
+        ...users,
+        fetching: true
+      });
+    }
   };
-
-  useEffect((): void => {
-    if (!rendered) {
-      setRendered(true);
-    }
-  }, []);
-
-  useEffect((): void => {
-    if (rendered && users.fetching && !users.fetched) {
-      breezySocket
-        .timeout(60000)
-        .emit(
-          'fetch users',
-          (socketError: Error, response: FetchUsersRes): void => {
-            setUsers({
-              ...users,
-              fetching: false,
-              fetched: !socketError,
-              list: socketError ? [...users.list] : response.data.users
-            });
-          }
-        );
-    }
-  }, [rendered, users]);
 
   return <Button onClick={(): void => handleRetryFetch()}>{label}</Button>;
 };
