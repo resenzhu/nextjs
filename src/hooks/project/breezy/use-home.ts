c45b2c86cf2c0ec26fafe2a173f43eceb1e97f1c
resenzhu <resen.zhu@gmail.com>
2023-11-13 15:18:43 +0700

    add Settings and setSettings

--- src/hooks/project/breezy/use-home.ts
+++ src/hooks/project/breezy/use-home.ts
@@ -2,10 +2,12 @@ import {
   type Menu,
   type Messages,
   type Profile,
+  type Settings,
   type Users,
   setMenu as setHomeMenu,
   setMessages as setHomeMessages,
   setProfile as setHomeProfile,
+  setSettings as setHomeSettings,
   setUsers as setHomeUsers
 } from '@redux/reducers/project/breezy/home';
 import {useDispatch, useSelector} from '@redux/hooks';
@@ -15,10 +17,12 @@ type UseHome = {
   messages: Messages;
   users: Users;
   profile: Profile;
+  settings: Settings;
   setMenu: (menu: Menu) => void;
   setMessages: (messages: Messages) => void;
   setUsers: (users: Users) => void;
   setProfile: (profile: Profile) => void;
+  setSettings: (settings: Settings) => void;
 };
 
 const useHome = (): UseHome => {
@@ -40,15 +44,21 @@ const useHome = (): UseHome => {
     dispatch(setHomeProfile(profile));
   };
 
+  const setSettings = (settings: Settings): void => {
+    dispatch(setHomeSettings(settings));
+  };
+
   return {
     menu: useSelector((state) => state.project.breezy.home.menu),
     messages: useSelector((state) => state.project.breezy.home.messages),
     users: useSelector((state) => state.project.breezy.home.users),
     profile: useSelector((state) => state.project.breezy.home.profile),
+    settings: useSelector((state) => state.project.breezy.home.settings),
     setMenu: setMenu,
     setMessages: setMessages,
     setUsers: setUsers,
-    setProfile: setProfile
+    setProfile: setProfile,
+    setSettings: setSettings
   };
 };
 
