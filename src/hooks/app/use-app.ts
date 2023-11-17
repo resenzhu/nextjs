c98be8d7e1b904f94a8201f5de2cbcadb1b6fbc7
Resen <resen.zhu@gmail.com>
2023-11-17 15:52:23 +0700

    add app hook

--- src/hooks/app/use-app.ts
+++ src/hooks/app/use-app.ts
@@ -1,20 +1,16 @@
 import {
   type Online,
-  type Sidenav,
   type Viewport,
   setOnline as setAppOnline,
-  setSidenav as setAppSidenav,
   setViewport as setAppViewport
-} from '@redux/reducers/main/app';
+} from '@redux/reducers/app';
 import {useDispatch, useSelector} from '@redux/hooks';
 
 type UseApp = {
   online: Online;
   viewport: Viewport;
-  sidenav: Sidenav;
   setOnline: (online: Online) => void;
   setViewport: (viewport: Viewport) => void;
-  setSidenav: (sidenav: Sidenav) => void;
 };
 
 const useApp = (): UseApp => {
@@ -28,17 +24,11 @@ const useApp = (): UseApp => {
     dispatch(setAppViewport(viewport));
   };
 
-  const setSidenav = (sidenav: Sidenav): void => {
-    dispatch(setAppSidenav(sidenav));
-  };
-
   return {
     online: useSelector((state) => state.main.app.online),
     viewport: useSelector((state) => state.main.app.viewport),
-    sidenav: useSelector((state) => state.main.app.sidenav),
     setOnline: setOnline,
-    setViewport: setViewport,
-    setSidenav: setSidenav
+    setViewport: setViewport
   };
 };
 
