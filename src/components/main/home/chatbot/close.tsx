2deb99b4093d412d73d54d6350c98ec0885d1cae
Resen <resen.zhu@gmail.com>
2023-11-19 15:21:19 +0700

    change app hook path

--- src/components/main/home/chatbot/close.tsx
+++ src/components/main/home/chatbot/close.tsx
@@ -2,7 +2,7 @@
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import type {IconDefinition} from '@fortawesome/free-solid-svg-icons';
-import useApp from '@hooks/main/use-app';
+import useApp from '@hooks/app/use-app';
 import useHome from '@hooks/main/use-home';
 
 type CloseProps = {
