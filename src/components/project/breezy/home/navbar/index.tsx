937def81d793eb0a425777d45c3ba397c50fd323
Resen <resen.zhu@gmail.com>
2023-12-03 20:46:33 +0700

    add breezy tnavbar transition

--- src/components/project/breezy/home/navbar/index.tsx
+++ src/components/project/breezy/home/navbar/index.tsx
@@ -1,6 +1,7 @@
 import type {IconDefinition} from '@fortawesome/free-solid-svg-icons';
 import NavMenu from '@components/project/breezy/home/navbar/navmenu';
 import type {ReactNode} from 'react';
+import {TNavbar} from '@components/project/breezy/home/navbar/transition';
 
 type NavbarProps = {
   children: ReactNode;
@@ -12,9 +13,11 @@ type MenuProps = {
 };
 
 const Navbar = ({children}: NavbarProps): JSX.Element => (
-  <nav className='fixed bottom-0 flex w-screen animate-fade-up justify-evenly border-t-2 bg-white shadow-sm animate-duration-700 md:relative md:h-full md:w-14 md:flex-col md:justify-normal md:border-r-2 md:border-t-0'>
-    {children}
-  </nav>
+  <TNavbar>
+    <nav className='fixed bottom-0 flex w-screen animate-fade-up justify-evenly border-t-2 bg-white shadow-sm animate-duration-700 md:relative md:h-full md:w-14 md:flex-col md:justify-normal md:border-r-2 md:border-t-0'>
+      {children}
+    </nav>
+  </TNavbar>
 );
 
 const Menu = ({name, icon}: MenuProps): JSX.Element => (
