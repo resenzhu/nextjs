227a35270179d3aea54567af1e270ebc9bd09e72
Resen <resen.zhu@gmail.com>
2023-12-10 13:49:33 +0700

    remove classname

--- src/components/project/breezy/shared/recaptcha2.tsx
+++ src/components/project/breezy/shared/recaptcha2.tsx
@@ -10,7 +10,6 @@ type RecaptchaV2Props = {
 
 const RecaptchaV2 = ({reference, onChange}: RecaptchaV2Props): JSX.Element => (
   <Recaptcha
-    className='place-self-center'
     ref={reference}
     sitekey={process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY_V2_CHECKBOX}
     onChange={onChange}
