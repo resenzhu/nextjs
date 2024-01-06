c624d91b0a352ce762b97606c9a2720ba0a50c6b
Resen <resen.zhu@gmail.com>
2024-01-06 13:51:55 +0700

    remove asyncScriptOnLoad prop

--- src/components/project/breezy/shared/recaptcha2.tsx
+++ src/components/project/breezy/shared/recaptcha2.tsx
@@ -5,19 +5,13 @@ import Recaptcha from 'react-google-recaptcha';
 
 type RecaptchaV2Props = {
   reference: LegacyRef<Recaptcha>;
-  asyncScriptOnLoad: () => void;
   onChange: (captchaToken: string | null) => void;
 };
 
-const RecaptchaV2 = ({
-  reference,
-  asyncScriptOnLoad,
-  onChange
-}: RecaptchaV2Props): JSX.Element => (
+const RecaptchaV2 = ({reference, onChange}: RecaptchaV2Props): JSX.Element => (
   <Recaptcha
     ref={reference}
     sitekey={process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY_V2_CHECKBOX}
-    asyncScriptOnLoad={asyncScriptOnLoad}
     onChange={onChange}
   />
 );
