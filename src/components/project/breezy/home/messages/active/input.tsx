dce372eaf64cfc9ee79204483434c89c4da015d6
Resen <resen.zhu@gmail.com>
2024-01-21 16:50:56 +0700

    add handleUpdateInput and handleSendInput events

--- src/components/project/breezy/home/messages/active/input.tsx
+++ src/components/project/breezy/home/messages/active/input.tsx
@@ -1,29 +1,110 @@
+'use client';
+
+import type {ChangeEvent, FormEvent} from 'react';
+import {Button} from '@components/project/breezy/shared';
+import {DateTime} from 'luxon';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import type {IconDefinition} from '@fortawesome/free-solid-svg-icons';
+import type {Message} from '@redux/reducers/project/breezy/dashboard';
+import {nanoid} from 'nanoid';
+import useDashboard from '@hooks/project/breezy/use-dashboard';
 
 type InputProps = {
   placeholder: string;
   sendIcon: IconDefinition;
 };
 
-const Input = ({placeholder, sendIcon}: InputProps): JSX.Element => (
-  <form className='mx-4 flex items-center justify-between space-x-3 py-3'>
-    <textarea
-      className='w-full flex-1 resize-none bg-gray-50 px-3 py-2 outline-0'
-      placeholder={placeholder}
-      rows={1}
-    />
-    <button
-      className='rounded-full bg-purple-500 p-1 duration-150 hover:bg-purple-600 disabled:bg-gray-300'
-      type='submit'
+const Input = ({placeholder, sendIcon}: InputProps): JSX.Element => {
+  const {messages, setMessages} = useDashboard();
+
+  const handleUpdateInput = (event: ChangeEvent<HTMLTextAreaElement>): void => {
+    const {value} = event.target;
+    if (
+      messages.list.find(
+        (message): boolean => message.sender.id === messages.active
+      )?.message !== value
+    ) {
+      setMessages({
+        ...messages,
+        list: messages.list.map((message): Message => {
+          if (message.sender.id === messages.active) {
+            const updatedMessage: Message = {
+              ...message,
+              message: value
+            };
+            return updatedMessage;
+          }
+          return message;
+        })
+      });
+    }
+  };
+
+  const handleSendInput = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    const activeMessage = messages.list.find(
+      (message): boolean => message.sender.id === messages.active
+    );
+    if (activeMessage && activeMessage.message.length !== 0) {
+      setMessages({
+        ...messages,
+        list: messages.list.map((message): Message => {
+          if (message.sender.id === messages.active) {
+            const updatedMessage: Message = {
+              ...message,
+              chats: [
+                ...message.chats,
+                {
+                  id: nanoid(),
+                  self: true,
+                  message: activeMessage.message,
+                  status: 'sending',
+                  timestamp: {
+                    created: DateTime.utc().toISO() ?? new Date().toISOString(),
+                    delivered: null,
+                    read: null
+                  }
+                }
+              ],
+              message: ''
+            };
+            return updatedMessage;
+          }
+          return message;
+        })
+      });
+    }
+  };
+
+  return (
+    <form
+      className='mx-4 flex items-center justify-between space-x-3 py-3'
+      onSubmit={handleSendInput}
     >
-      <FontAwesomeIcon
-        className='w-6 text-white'
-        icon={sendIcon}
+      <textarea
+        className='w-full flex-1 resize-none bg-gray-50 px-3 py-2 outline-0'
+        placeholder={placeholder}
+        rows={1}
+        value={
+          messages.list.find(
+            (message): boolean => message.sender.id === messages.active
+          )?.message
+        }
+        maxLength={4096}
+        onChange={(event): void => handleUpdateInput(event)}
       />
-    </button>
-  </form>
-);
+      <Button
+        className='px-[0.5rem]'
+        type='submit'
+      >
+        <FontAwesomeIcon
+          className='w-6 text-white'
+          icon={sendIcon}
+        />
+      </Button>
+    </form>
+  );
+};
 
 export type {InputProps};
 export default Input;
