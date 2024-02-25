1a422f2ee23d6cef44ca73da9bf29657a27ee63b
Resen <resen.zhu@gmail.com>
2024-02-25 21:17:53 +0700

    remove breezy navbar transition

--- src/components/project/breezy/home/navbar/index.tsx
+++ src/components/project/breezy/home/navbar/index.tsx
@@ -1,7 +1,6 @@
 import type {IconDefinition} from '@fortawesome/free-solid-svg-icons';
 import NavMenu from '@components/project/breezy/home/navbar/navmenu';
 import type {ReactNode} from 'react';
-import {TNavbar} from '@components/project/breezy/home/navbar/transition';
 
 type NavbarProps = {
   children: ReactNode;
@@ -13,11 +12,9 @@ type MenuProps = {
 };
 
 const Navbar = ({children}: NavbarProps): JSX.Element => (
-  <TNavbar>
-    <nav className='fixed bottom-0 flex w-screen animate-fade-up justify-evenly border-t-2 bg-white shadow-sm animate-duration-700 md:relative md:h-full md:w-14 md:flex-col md:justify-normal md:border-r-2 md:border-t-0'>
-      {children}
-    </nav>
-  </TNavbar>
+  <nav className='fixed bottom-0 flex w-screen animate-fade-up justify-evenly border-t-2 bg-white shadow-sm animate-duration-700 md:relative md:h-full md:w-14 md:flex-col md:justify-normal md:border-r-2 md:border-t-0'>
+    {children}
+  </nav>
 );
 
 const Menu = ({name, icon}: MenuProps): JSX.Element => (
